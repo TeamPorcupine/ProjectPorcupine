9995ff0e0a3c650fec15982c63e3adf3bfc57702
Olov Johansson <johansson.olov@gmail.com>
2016-08-17 21:32:38 +0200

    Cleanup

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,7 +3,7 @@
 # Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
 
 # Change this the name of your project. This will be the name of the final executables as well.
-project="ci-build"
+project="project-porcupine"
 
 echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
@@ -17,3 +17,5 @@ echo "Attempting to build $project for Windows"
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
+
+#removed windows and linux builds. Perhaps they can be fixed
--- Scripts/install.sh
+++ Scripts/install.sh
@@ -4,6 +4,8 @@
 
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
+
+# perhaps a way to always get latest or else this has to be bumped every time a new version of unity is released
 echo 'Downloading Unity-5.4.0f3: '
 curl -o Unity.pkg http://download.unity3d.com/download_unity/a6d8d714de6f/MacEditorInstaller/Unity-5.4.0f3.pkg
 
