e26dc4ab8458f172a17ccd4aff9ab58ec6db5680
Olov Johansson <johansson.olov@gmail.com>
2016-08-17 20:32:32 +0200

    correct version

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -4,8 +4,8 @@
 
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
-echo 'Downloading from http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg: '
-curl -o Unity.pkg http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg
+echo 'Downloading Unity-5.4.0f3: '
+curl -o Unity.pkg http://download.unity3d.com/download_unity/a6d8d714de6f/MacEditorInstaller/Unity-5.4.0f3.pkg
 
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
