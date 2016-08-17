a8ee5909ca03ef2d38ad302a8ea02fa094699403
AE4 <Alfreds@post.com>
2016-08-17 19:56:37 +0200

    Fixed The Issue

--- Assets/Scripts/UI/MouseOverTileTypeText.cs
+++ Assets/Scripts/UI/MouseOverTileTypeText.cs
@@ -36,6 +36,8 @@ public class MouseOverTileTypeText : MonoBehaviour
     void Update()
     {
         Tile t = mouseController.GetMouseOverTile();
-        myText.text = "Tile Type: " + t.Type.ToString();
+
+        if (t != null)
+            myText.text = "Tile Type: " + t.Type.ToString();
     }
 }
