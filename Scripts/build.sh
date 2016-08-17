72d1d15a689c54ad412cffa5fb51fe52cdbc7e7d
Olov Johansson <johansson.olov@gmail.com>
2016-08-17 21:37:21 +0200

    build only macos

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -5,14 +5,14 @@
 # Change this the name of your project. This will be the name of the final executables as well.
 project="project-porcupine"
 
-echo "Attempting to build $project for Windows"
+echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
 
 echo 'Logs from build'
