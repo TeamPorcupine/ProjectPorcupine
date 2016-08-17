b58d585c5556bb8cc32fb606acecac77461f2e25
relinu <xrelinux@yahoo.de>
2016-08-18 00:32:08 +0200

    Added pause menu - Press esc

---
+++ Assets/Scripts/UI/Menus/EscMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class EscMenu : MonoBehaviour {
+
+    public Canvas canvas;
+
+	void Start () {
+        canvas = GetComponent<Canvas>();
+    }
+	
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            ToggleMenu();
+        }
+	}
+
+    public void ToggleMenu() {
+        canvas.enabled = !canvas.enabled;
+        WorldController.Instance.IsModal = canvas.enabled;
+    }
+}
