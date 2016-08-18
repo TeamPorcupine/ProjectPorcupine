e6732d62e31ee27b81e958ebe3fe39bf1aafbc6c
Emil <emilkris33@gmail.com>
2016-08-18 10:26:51 +0200

    Revert "Fixed lua callback error"

--- Assets/StreamingAssets/LUA/Furniture.lua
+++ Assets/StreamingAssets/LUA/Furniture.lua
@@ -43,7 +43,11 @@ function OnUpdate_Door( furniture, deltaTime )
 
 	furniture.SetParameter("openness", Clamp01(furniture.GetParameter("openness")) )
 
-	furniture.UpdateOnChanged(furniture);
+	if (furniture.cbOnChanged != nil) then
+		furniture.cbOnChanged(furniture)
+	end
+
+
 end
 
 function IsEnterable_Door( furniture )
