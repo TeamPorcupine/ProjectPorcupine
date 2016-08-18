146deff4b4fd1fc70ce5277ce28187452c87a300
Kolja Nagel <ko.nagel2000@gmail.com>
2016-08-18 22:01:24 +0200

    formatting

--- Assets/UI/DialogListItem.cs
+++ Assets/UI/DialogListItem.cs
@@ -5,21 +5,21 @@ using System.Collections;
 
 public class DialogListItem : MonoBehaviour, IPointerClickHandler
 {
-    public string fileName;
-    public InputField inputField;
+	public string fileName;
+	public InputField inputField;
 
-    #region IPointerClickHandler implementation
+	#region IPointerClickHandler implementation
 
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        // Our job is to take our text label and 
-        // copy it into a target field.
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		// Our job is to take our text label and 
+		// copy it into a target field.
 
-        inputField.text = fileName;
-        GameObject go = GameObject.FindGameObjectWithTag("DeleteButton");
-        go.GetComponent<Image>().color = new Color(255, 255, 255, 255);
-        go.transform.position = new Vector3(transform.GetComponentInChildren<Text>().transform.position.x + 110f, transform.GetComponentInChildren<Text>().transform.position.y - 8f);
-    }
+		inputField.text = fileName;
+		GameObject go = GameObject.FindGameObjectWithTag("DeleteButton");
+		go.GetComponent<Image>().color = new Color(255, 255, 255, 255);
+		go.transform.position = new Vector3(transform.GetComponentInChildren<Text>().transform.position.x + 110f, transform.GetComponentInChildren<Text>().transform.position.y - 8f);
+	}
 
-    #endregion
+	#endregion
 }
