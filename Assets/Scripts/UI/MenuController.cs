a0a3838fafb3d2dc5f039e02bf36a7cd444f2ee2
buckeyestudios <buckeyestudio@gmail.com>
2016-08-19 16:01:26 -0400

    Changed UI - Menus
    Changed the way the UI menus are. Now they are similar to RimWorld

---
+++ Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	public GameObject menu_Furniture;
+	public GameObject menu_Floor;
+	public GameObject menu_Options;
+	public GameObject menu_Settings;
+
+	// Use this for initialization
+	void Start () {
+		DeactivateMenus ();
+	}
+	
+
+	public void DeactivateMenus()
+	{
+		menu_Furniture.SetActive (false);
+		menu_Floor.SetActive(false);
+		menu_Options.SetActive(false);
+		menu_Settings.SetActive(false);
+	}
+
+
+}
