9fdc5bad52ffec507ed83b04a2f50c09a4cbc833
Jack Evans <jack_evans@gmx.com>
2016-08-19 14:41:22 +0100

    All TopMenu buttons from code, still no listeners.

--- Assets/Scripts/Controllers/UIController.cs
+++ Assets/Scripts/Controllers/UIController.cs
@@ -16,6 +16,9 @@ public class UIController : MonoBehaviour
 //        }
         GameObject topMenu = GameObject.Find("TopMenu");
         GameObject newWorld = Instantiate(Resources.Load("Button - New World"), topMenu.transform) as GameObject;
+        GameObject load = Instantiate(Resources.Load("Button - Load"), topMenu.transform) as GameObject;
+        GameObject save = Instantiate(Resources.Load("Button - Save"), topMenu.transform) as GameObject;
+        GameObject pathTest = Instantiate(Resources.Load("Button - Pathfinding Test"), topMenu.transform) as GameObject;
 	}
 	
 	// Update is called once per frame
