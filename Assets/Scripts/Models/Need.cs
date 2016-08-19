a0f3c4b6c7bb9ddb28413512ed3a3680b91c634e
Dan54 <dannyjwheeler@hotmail.co.uk>
2016-08-19 19:24:18 +0100

    Added need class

---
+++ Assets/Scripts/Models/Need.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Need {
+	public string needType;
+	public string Name;
+	float growthRate;
+	public float Amount;
+	bool invertDisplay;
+	public string DisplayAmount
+	{
+		get
+		{
+			if (invertDisplay)
+			{
+				return 100 - amount + "%";
+			}
+			return amount + "%";
+		}
+	}
+	bool completeOnFail;
+	float addedInVaccum;
+	float dps;
+	// Use this for initialization
+	public Need () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update (float deltaTime) {
+	
+	}
+}
