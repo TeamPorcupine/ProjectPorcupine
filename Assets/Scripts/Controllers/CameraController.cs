6539ff2c16d48cb446bb34bdfdac42e8b469ceef
Filippo Leonardi <filippo.leon@gmail.com>
2016-08-19 15:50:42 +0200

    Fix indentation

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -5,16 +5,17 @@ public class CameraController : MonoBehaviour {
 
     [Range(0, 3)]
     public float scrollSpeed = 0.1f;
-    
-    void Start () {
 	
-	}
-	
-	void Update () {
+    void Update () {
         
         // React to hor./vert. axis (WASD or up/down/...)
-        Camera.main.transform.position += Camera.main.orthographicSize * scrollSpeed * new Vector3(Input.GetAxis("Horizontal"),
-                                                                        Input.GetAxis("Vertical"), 0);
+        Camera.main.transform.position +=
+            Camera.main.orthographicSize * scrollSpeed *
+            new Vector3(
+                Input.GetAxis("Horizontal"),
+                Input.GetAxis("Vertical"),
+                0
+            );
 
     }
 }
