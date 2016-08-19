273821d651357ee25db6f9a9dc0a5026b9d5b2fc
Koljenka <ko.nagel2000@gmail.com>
2016-08-19 03:52:53 +0200

    Delete button (#194)
    * redone delete button
    
    * Revert "redone delete button"
    
    This reverts commit dc065eee0a753f57a1658253c14641fb41f8493f.
    
    * redone and fixed
    
    * formatting
    
    * added Are you sure dialog
    
    * removed typo
    
    * minor changes
    
    * fixed anchors
    
    * the delete button now scrolls with the selected file

--- Assets/Scripts/UI/DialogBox/FileSaveLoad/DialogBoxLoadGame.cs
+++ Assets/Scripts/UI/DialogBox/FileSaveLoad/DialogBoxLoadGame.cs
@@ -3,56 +3,125 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Xml.Serialization;
 using System.IO;
+using UnityEditor;
 
 
 public class DialogBoxLoadGame : DialogBoxLoadSaveGame
 {
-    public void OkayWasClicked()
-    {
-        string fileName = gameObject.GetComponentInChildren<InputField>().text;
+	
+	public GameObject dialog;
+	GameObject go;
+	public bool pressedDelete;
+	Component fileItem;
 
-        // TODO: Is the filename valid?  I.E. we may want to ban path-delimiters (/ \ or :) and 
-        // maybe periods?      ../../some_important_file
+	void Update()
+	{
+		if (pressedDelete)
+		{
+			SetButtonLocation(fileItem);
+		}
+	}
 
-        // Right now fileName is just what was in the dialog box.  We need to pad this out to the full
-        // path, plus an extension!
-        // In the end, we're looking for something that's going to be similar to this (depending on OS)
-        //    C:\Users\Quill18\ApplicationData\MyCompanyName\MyGameName\Saves\SaveGameName123.sav
+	public void SetFileItem(Component item)
+	{
+		fileItem = item;
+	}
 
-        // Application.persistentDataPath == C:\Users\<username>\ApplicationData\MyCompanyName\MyGameName\
+	public void SetButtonLocation(Component item)
+	{
+		GameObject go = GameObject.FindGameObjectWithTag("DeleteButton");
+		go.transform.position = new Vector3(item.transform.position.x + 110f, item.transform.position.y - 8f);
+	}
 
-        string saveDirectoryPath = WorldController.Instance.FileSaveBasePath();
+	public void OkayWasClicked()
+	{
+		
+		string fileName = gameObject.GetComponentInChildren<InputField>().text;
+		// TODO: Is the filename valid?  I.E. we may want to ban path-delimiters (/ \ or :) and 
+		// maybe periods?      ../../some_important_file
 
-        EnsureDirectoryExists(saveDirectoryPath);
+		// Right now fileName is just what was in the dialog box.  We need to pad this out to the full
+		// path, plus an extension!
+		// In the end, we're looking for something that's going to be similar to this (depending on OS)
+		//    C:\Users\Quill18\ApplicationData\MyCompanyName\MyGameName\Saves\SaveGameName123.sav
 
-        string filePath = System.IO.Path.Combine(saveDirectoryPath, fileName + ".sav");
+		// Application.persistentDataPath == C:\Users\<username>\ApplicationData\MyCompanyName\MyGameName\
 
-        // At this point, filePath should look very much like
-        //     C:\Users\Quill18\ApplicationData\MyCompanyName\MyGameName\Saves\SaveGameName123.sav
+		string saveDirectoryPath = WorldController.Instance.FileSaveBasePath();
 
-        if (File.Exists(filePath) == false)
-        {
-            // TODO: Do file overwrite dialog box.
+		EnsureDirectoryExists(saveDirectoryPath);
 
-            Debug.LogError("File doesn't exist.  What?");
-            CloseDialog();
-            return;
-        }
+		string filePath = System.IO.Path.Combine(saveDirectoryPath, fileName + ".sav");
 
-        CloseDialog();
+		// At this point, filePath should look very much like
+		//     C:\Users\Quill18\ApplicationData\MyCompanyName\MyGameName\Saves\SaveGameName123.sav
 
-        LoadWorld(filePath);
-    }
+		if (File.Exists(filePath) == false)
+		{
+			// TODO: Do file overwrite dialog box.
 
-    public void LoadWorld(string filePath)
-    {
-        // This function gets called when the user confirms a filename
-        // from the load dialog box.
+			Debug.LogError("File doesn't exist.  What?");
+			CloseDialog();
+			return;
+		}
 
-        // Get the file name from the save file dialog box
+		CloseDialog();
 
-        Debug.Log("LoadWorld button was clicked.");
+		LoadWorld(filePath);
+	}
 
-        WorldController.Instance.LoadWorld(filePath);
-    }
+	public override void CloseDialog()
+	{
+		GameObject go = GameObject.FindGameObjectWithTag("DeleteButton");
+		go.GetComponent<Image>().color = new Color(255, 255, 255, 0);
+		pressedDelete=false;
+		base.CloseDialog();
+	}
+
+	public void DeleteFile()
+	{
+		string fileName = gameObject.GetComponentInChildren<InputField>().text;
+
+		string saveDirectoryPath = WorldController.Instance.FileSaveBasePath();
+
+		EnsureDirectoryExists(saveDirectoryPath);
+
+		string filePath = System.IO.Path.Combine(saveDirectoryPath, fileName + ".sav");
+
+		if (File.Exists(filePath) == false)
+		{
+
+			Debug.LogError("File doesn't exist.  What?");
+			CloseDialog();
+			return;
+		}
+
+		CloseSureDialog();
+		FileUtil.DeleteFileOrDirectory(filePath);
+		CloseDialog();
+		ShowDialog();
+	}
+
+	public void CloseSureDialog()
+	{
+		dialog.SetActive(false);
+	}
+
+	public void DeleteWasClicked()
+	{
+		
+		dialog.SetActive(true);
+	}
+
+	public void LoadWorld(string filePath)
+	{
+		// This function gets called when the user confirms a filename
+		// from the load dialog box.
+
+		// Get the file name from the save file dialog box
+
+		Debug.Log("LoadWorld button was clicked.");
+
+		WorldController.Instance.LoadWorld(filePath);
+	}
 }
