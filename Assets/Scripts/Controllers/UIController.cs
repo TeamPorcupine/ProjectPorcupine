d6463fcdccd5e7971d6d3b4fc3fc11d45508a610
Jack Evans <jack_evans@gmx.com>
2016-08-19 12:04:01 +0100

    Created UI controller script and object. Empty right now.

---
+++ Assets/Scripts/Controllers/UIController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
