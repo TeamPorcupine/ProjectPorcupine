1cd868efc854d456aed1b78ff657190894dc0590
Dan54 <dannyjwheeler@hotmail.co.uk>
2016-08-19 19:46:20 +0100

    expanded need

--- Assets/Scripts/Models/Need.cs
+++ Assets/Scripts/Models/Need.cs
@@ -7,27 +7,35 @@ public class Need {
 	float growthRate;
 	public float Amount;
 	bool invertDisplay;
+	public Character character;
 	public string DisplayAmount
 	{
 		get
 		{
 			if (invertDisplay)
 			{
-				return 100 - amount + "%";
+				return (100 - (int)Amount) + "%";
 			}
-			return amount + "%";
+			return ((int)Amount) + "%";
 		}
 	}
 	bool completeOnFail;
 	float addedInVaccum;
 	float dps;
 	// Use this for initialization
-	public Need () {
+	public Need ()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update (float deltaTime) {
-	
+	public void Update (float deltaTime)
+	{
+		Amount += growthRate * deltaTime;
+		if (character != null && character.CurrTile.room.GetGasPressure ("O2") < 0.15)
+		{
+			Amount += (addedInVaccum - (addedInVaccum * (character.CurrTile.room.GetGasPressure ("O2") / 0.2))) * deltaTime;
+		}
+		//TODO: What happens at 100%?
 	}
 }
