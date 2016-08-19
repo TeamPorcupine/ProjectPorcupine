64ab44f0ffdefa29fbd90191e4d39eb2a1fea67f
Jack Evans <jack_evans@gmx.com>
2016-08-19 12:44:41 +0100

    Made a UI controller that can now instantiate prefab UI elements. Currently doesn't handle OnClick.

--- Assets/Scripts/Controllers/UIController.cs
+++ Assets/Scripts/Controllers/UIController.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class UIController : MonoBehaviour {
+public class UIController : MonoBehaviour 
+{
+    public GameObject[] Button;
+    public GameObject ParentObj;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+        int a = Button.Length;
+        for (int i = 0; i < a; i++)
+        {
+            Instantiate(Button[i], ParentObj.transform);
+        }
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+	    
 	}
 }
