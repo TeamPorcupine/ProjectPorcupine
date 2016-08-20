134694ebedf6c782ed6aea9274c101494caa0e42
Dan54 <dannyjwheeler@hotmail.co.uk>
2016-08-20 09:47:07 +0100

    XML Reading and refactoring

--- Assets/Scripts/Models/Need.cs
+++ Assets/Scripts/Models/Need.cs
@@ -1,29 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
 
 public class Need {
 	public string needType;
 	public string Name;
-	float growthRate;
-	public float Amount;
-	bool invertDisplay;
+	protected float growthRate;
+	public float Amount = 0;
+	protected bool highToLow = true;
 	public Character character;
-	Furniture restoreNeedFurn;
-	float restoreNeedTime;
+	protected Furniture restoreNeedFurn;
+	protected float restoreNeedTime;
 	public string DisplayAmount
 	{
 		get
 		{
-			if (invertDisplay)
+			if (highToLow)
 			{
 				return (100 - (int)Amount) + "%";
 			}
 			return ((int)Amount) + "%";
 		}
 	}
-	bool completeOnFail;
-	float addedInVaccum;
-	float dps;
+	protected bool completeOnFail;
+	protected float addedInVacuum;
+	protected float dps;
 	// Use this for initialization
 	public Need ()
 	{
@@ -36,8 +39,59 @@ public class Need {
 		Amount += growthRate * deltaTime;
 		if (character != null && character.CurrTile.room.GetGasPressure ("O2") < 0.15)
 		{
-			Amount += (addedInVaccum - (addedInVaccum * (character.CurrTile.room.GetGasPressure ("O2") / 0.2))) * deltaTime;
+			Amount += (addedInVacuum - (addedInVacuum * (character.CurrTile.room.GetGasPressure ("O2") * 5))) * deltaTime;
 		}
 		//TODO: What happens at 100%?
+	}
+
+	public void ReadXmlPrototype(XmlReader reader_parent)
+	{
+		//Debug.Log("ReadXmlPrototype");
+
+		needType = reader_parent.GetAttribute("needType");
+
+		XmlReader reader = reader_parent.ReadSubtree();
+
+
+		while (reader.Read())
+		{
+			switch (reader.Name)
+			{
+			case "Name":
+				reader.Read();
+				Name = reader.ReadContentAsString();
+				break;
+			case "RestoreNeedFurnitureType":
+				reader.Read();
+				restoreNeedFurn = World.current.furniturePrototypes[reader.ReadContentAsString()];
+				break;
+			case "RestoreNeedTime":
+				reader.Read();
+				restoreNeedTime = reader.ReadContentAsFloat();
+				break;
+			case "Damage":
+				reader.Read();
+				dps = reader.ReadContentAsFloat();
+				break;
+			case "CompleteOnFail":
+				reader.Read();
+				completeOnFail = reader.ReadContentAsBoolean();
+				break;
+			case "HighToLow":
+				reader.Read();
+				highToLow = reader.ReadContentAsBoolean();
+				break;
+			case "GrowthRate":
+				reader.Read ();
+				growthRate = reader.ReadContentAsFloat();
+				break;
+			case "GrowthInVacuum":
+				reader.Read ();
+				addedInVacuum = reader.ReadContentAsFloat();
+				break;
+			}
+		}
+
+
 	}
 }
