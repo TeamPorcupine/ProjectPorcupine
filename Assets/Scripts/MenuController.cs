5dbb7d6e702f1844d1381cff7dd7b1f9f2ed753e
Dustin Loring <dustinwloring1988@gmail.com>
2016-08-20 07:44:36 -0400

    Menu Script

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+	public GameObject constructorMenu;
+
+	public GameObject optionsMenu;
+	public GameObject settingsMenu;
+
+	public GameObject furnitureMenu;
+	public GameObject floorMenu;
+
+	// Use this for initialization
+	void Start () {
+		DeactivateAll ();
+	}
+	
+	public void DeactivateAll () {
+		DeactivateConstructor ();
+		settingsMenu.SetActive (false);
+		optionsMenu.SetActive (false);
+
+	}
+
+	public void DeactivateConstructor () {
+		DeactivateSubs ();
+		constructorMenu.SetActive (false);
+
+	}
+
+	public void DeactivateSubs () {
+		furnitureMenu.SetActive (false);
+		floorMenu.SetActive (false);
+	}
+}
