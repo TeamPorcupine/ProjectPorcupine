a742ec635e472bcd21648733dc4af73b9c7c603d
dustinwloring1988 <dustinwloring1988@gmail.com>
2016-08-20 08:41:42 -0400

    Setting Panel Added
    Now we have some setting and a FPS counter

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -2,32 +2,37 @@
 using System.Collections;
 
 public class MenuController : MonoBehaviour {
+	//The left build menu
 	public GameObject constructorMenu;
 
-	public GameObject optionsMenu;
-	public GameObject settingsMenu;
-
+	//The sub menus of the build menu (furniture, floor..... later - power, security, drones)
 	public GameObject furnitureMenu;
 	public GameObject floorMenu;
+
+	//The options and settings
+	public GameObject optionsMenu;
+	public GameObject settingsMenu;
 
 	// Use this for initialization
 	void Start () {
 		DeactivateAll ();
 	}
-	
+
+	//Deactivates All Menus
 	public void DeactivateAll () {
 		DeactivateConstructor ();
 		settingsMenu.SetActive (false);
 		optionsMenu.SetActive (false);
-
 	}
 
+	//Deactivates All Menus Except the settings and options
 	public void DeactivateConstructor () {
 		DeactivateSubs ();
 		constructorMenu.SetActive (false);
 
 	}
 
+	//Deactivates any sub menu of the constrution options
 	public void DeactivateSubs () {
 		furnitureMenu.SetActive (false);
 		floorMenu.SetActive (false);
