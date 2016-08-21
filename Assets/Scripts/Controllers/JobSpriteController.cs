e9206c1f732dda916019f0653a707ac8fe8a8f88
Jan Göttlich <j.goettlich@hotmail.de>
2016-08-21 18:51:47 +0200

    Update JobSpriteController.cs

--- Assets/Scripts/Controllers/JobSpriteController.cs
+++ Assets/Scripts/Controllers/JobSpriteController.cs
@@ -22,10 +22,10 @@ public class JobSpriteController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-		jobGameObjectMap = new Dictionary<Job, GameObject>();
-		fsc = GameObject.FindObjectOfType<FurnitureSpriteController>();
+        jobGameObjectMap = new Dictionary<Job, GameObject>();
+        fsc = GameObject.FindObjectOfType<FurnitureSpriteController>();
 
-		WorldController.Instance.world.jobQueue.cbJobCreated += OnJobCreated;
+        WorldController.Instance.world.jobQueue.cbJobCreated += OnJobCreated;
     }
 
     void OnJobCreated(Job job)
