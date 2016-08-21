45b61ac26f5f79b9ff0c0efdb8c248259e09d19b
dustinwloring1988 <dustinwloring1988@gmail.com>
2016-08-21 11:52:44 -0400

    Setting Up for XML UI
    Setting up for XML UI. Create prefabs for it to spawn, created xml file,
    and created a test scene.

---
+++ Assets/XML_UI/Scripts/UIObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
