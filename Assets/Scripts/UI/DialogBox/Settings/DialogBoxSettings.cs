01e84c189684e4301022f906aab8ed7962e65f45
Dustin Loring <dustinwloring1988@gmail.com>
2016-08-21 02:11:59 -0400

    Code Driven UI - Please Commit (#472)
    * Last_Scene_UI
    
    * Fied
    
    * Updated to be more code driven
    
    Added some more addlistener instead of setting in the inspector
    
    * Update
    
    * Code Driven UI
    
    * Spelling typos
    
    Fixed
    
    * Add Listeners
    
    Started the refactoring of the UI code
    
    * Spelling
    
    * Spelling and spaces
    
    Fixed spelling and added spaces after "//" for comments
    
    * Added a Dialog Manager
    
    Added a dialog manager and some other small changed to code witch use
    it.
    
    * Changed RootMenuController name
    
    Changed RootMenuController name back to MenuController
    
    * Fixed comments
    
    * Save
    
    * Delete Double
    
    * Update Fix
    
    * Reformated some scripts
    
    * Changed Main Scene

---
+++ Assets/Scripts/UI/DialogBox/Settings/DialogBoxSettings.cs
@@ -0,0 +1,177 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DialogBoxSettings : DialogBox
+{
+
+    // langage Option.
+    public Toggle langToggle;
+    public GameObject langDropDown;
+
+    // FPS Option.
+    public Toggle fpsToggle;
+    public GameObject fpsObject;
+
+    public Toggle fullScreenToggle;
+
+    public Slider musicVolume;
+
+    public Resolution[] myResolutions;
+    public Dropdown resolutionDropdown;
+
+    public Dropdown aliasingDropdown;
+    public Dropdown vSyncDropdown;
+    public Dropdown qualityDropdown;
+
+    public GameSettings gameSettings;
+
+    public Button closeButton;
+    public Button saveButton;
+
+
+    void OnEnable()
+    {
+        // Create an instance of this class.
+        gameSettings = new GameSettings();
+
+        // Get all avalible resolution for the display.
+        myResolutions = Screen.resolutions;
+
+        // Add our listeners.
+        closeButton.onClick.AddListener(delegate
+            {
+                OnClickClose();
+            });
+        saveButton.onClick.AddListener(delegate
+            {
+                OnClickSave();
+            });
+
+        fpsToggle.onValueChanged.AddListener(delegate
+            {
+                OnFPSToggle();
+            });
+        langToggle.onValueChanged.AddListener(delegate
+            {
+                OnLangageToggle();
+            });
+        fullScreenToggle.onValueChanged.AddListener(delegate
+            {
+                OnFullScreenToggle();
+            });
+
+        resolutionDropdown.onValueChanged.AddListener(delegate
+            {
+                OnResolutionChange();
+            });
+        aliasingDropdown.onValueChanged.AddListener(delegate
+            {
+                OnAliasingChange();
+            });
+        vSyncDropdown.onValueChanged.AddListener(delegate
+            {
+                OnVSyncChange();
+            });
+        qualityDropdown.onValueChanged.AddListener(delegate
+            {
+                OnQualityChange();
+            });
+
+        musicVolume.onValueChanged.AddListener(delegate
+            {
+                OnMusicChange();
+            });
+
+        // Create the drop down for resolution.
+        CreateResDropDown();
+
+        // Load the setting.
+        LoadSetting();
+
+    }
+
+    public void OnLangageToggle()
+    {
+        gameSettings.isVisibleLangage = langToggle.isOn;
+        langDropDown.SetActive(langToggle.isOn);
+    }
+
+    public void OnFPSToggle()
+    {
+        gameSettings.isVisibleFPS = fpsToggle.isOn;
+        fpsObject.SetActive(fpsToggle.isOn);
+    }
+
+    public void OnFullScreenToggle()
+    {
+        gameSettings.isFullscreen = Screen.fullScreen = fullScreenToggle.isOn;
+    }
+
+    public void OnQualityChange()
+    {
+        gameSettings.qualityIndex = QualitySettings.masterTextureLimit = qualityDropdown.value;
+    }
+
+    public void OnVSyncChange()
+    {
+        gameSettings.vSyncIndex = QualitySettings.vSyncCount = vSyncDropdown.value;
+    }
+
+    public void OnResolutionChange()
+    {
+        gameSettings.resolutionIndex = resolutionDropdown.value;
+        // Need to make work.
+    }
+
+    public void OnAliasingChange()
+    {
+        gameSettings.antiAliasingIndex = QualitySettings.antiAliasing = aliasingDropdown.value;
+    }
+
+
+    public void OnMusicChange()
+    {
+
+    }
+
+    public void SaveSetting()
+    {
+
+    }
+
+    public void OnClickClose()
+    {
+        this.CloseDialog();
+    }
+
+    public void OnClickSave()
+    {
+        this.CloseDialog();
+        SaveSetting();
+    }
+
+    void LoadSetting()
+    {
+        aliasingDropdown.value = QualitySettings.antiAliasing;
+        vSyncDropdown.value = QualitySettings.vSyncCount;
+        qualityDropdown.value = QualitySettings.masterTextureLimit;
+
+        fullScreenToggle.isOn = Screen.fullScreen;
+        fpsToggle.isOn = fpsObject.activeSelf;
+        langToggle.isOn = langDropDown.activeSelf;
+    }
+
+    void CreateResDropDown()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            this.CloseDialog();
+        }
+    }
+}
