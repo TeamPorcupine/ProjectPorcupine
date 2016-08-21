ed49de9d9e65c0a2a5b89041fef535cd17c74393
Filippo Leonardi <filippo.leon@gmail.com>
2016-08-21 18:09:39 +0200

    Improved public interface

--- Assets/Scripts/Models/Temperature.cs
+++ Assets/Scripts/Models/Temperature.cs
@@ -12,27 +12,35 @@ using MoonSharp.Interpreter;
 public class Temperature
 {
     /// <summary>
-    /// All heaters and refrigerators shoul register an Action here
-    /// TODO: find elegant way to modify this through LUA
-    /// </summary>
-    public Action sinksAndSources;
-
-    /// <summary>
     /// How often doe the physics update
     /// </summary>
     public float updateInterval = 0.1f;
+
+    /// <summary>
+    /// Default value assigned to thermalDIffusivity at "empty" tile.
+    /// DO NOT TOUCH UNLESS YOU KNOW WHAT YOU ARE DOING: MUST BE BETWEEN 0 and 1!
+    /// </summary>
+    static public float defaultThermalDiffusivity = 1f;
+
+    ///// Private stuff
+
+    /// <summary>
+    /// All heaters and refrigerators shoul register themselves here using the public interface
+    /// </summary>
+    Dictionary<Furniture, Action> sinksAndSources;
+
+    /// <summary>
+    /// All heaters and refrigerators shoul register an Action here
+    /// TODO: find elegant way to modify this through LUA
+    /// </summary>
+    Action sinksAndSourcesActions;
 
     /// <summary>
     /// Internal only variables
     /// </summary>
     float[][] temperature;
     float[] thermalDiffusivity;
-
-    /// <summary>
-    /// Default value assigned to thermalDIffusivity at "empty" tile.
-    /// </summary>
-    static public float defaultThermalDiffusivity = 1f;
-
+    
     // Internal stuff
     /// <summary>
     /// Size of map
@@ -76,7 +84,7 @@ public class Temperature
         // TODO: remove, dummy heater at x=50, y=50, with power=1000
         //sinksAndSources += () => { SetTemperature(50, 50, -GetTemperature(50, 50) + 300); };
 
-        sinksAndSources += () => { SetTemperature(52, 52,  300); };
+        sinksAndSourcesActions += () => { SetTemperature(52, 52,  300); };
     }
 
     /// <summary>
@@ -100,6 +108,23 @@ public class Temperature
         }
     }
 
+    public void RegisterSinkOrSource(Furniture provider)
+    {
+        // TODO: This need to be implemented
+        //sinksAndSources[provider] = () => { provider.TriggerEvent("OnUpdateTemperature"); };
+
+        sinksAndSourcesActions += sinksAndSources[provider];
+    }
+
+    public void DeregisterSinkOrSource(Furniture provider)
+    {
+        if (sinksAndSources.ContainsKey(provider))
+        {
+            sinksAndSourcesActions -= sinksAndSources[provider];
+            sinksAndSources.Remove(provider);
+        }
+    }
+
     /// <summary>
     /// Public interface to temperature model, returns temperature at x,y
     /// </summary>
@@ -119,7 +144,23 @@ public class Temperature
     /// <param name="temp">temeprature to set at x,y</param>
     public void SetTemperature(int x, int y, float temp)
     {
-        temperature[offset][GetIndex(x, y)] = temp;
+        if (IsWithinTemperatureBounds(temp))
+        {
+            temperature[offset][GetIndex(x, y)] = temp;
+        }
+    }
+
+    /// <summary>
+    /// Public interface to changing the temperature, increases temperature at (x,y) by incr
+    /// </summary>
+    /// <param name="x">y coord</param>
+    /// <param name="y">y coord</param>
+    /// <param name="incr">temeprature to set at x,y</param>
+    public void ChangeTemperature(int x, int y, float incr)
+    {
+        if (IsWithinTemperatureBounds(temperature[offset][GetIndex(x, y)] + incr)) {
+            temperature[offset][GetIndex(x, y)] += incr;
+        }
     }
 
     /// <summary>
@@ -144,8 +185,62 @@ public class Temperature
     /// <param name="coeff">thermal diffusifity to set at x,y</param>
     public void SetThermalDiffusivity(int x, int y, float coeff)
     {
-        thermalDiffusivity[GetIndex(x, y)] = coeff;
+        if (IsWithinThermalDiffusivityBounds(coeff))
+        {
+            thermalDiffusivity[GetIndex(x, y)] = coeff;
+        }
     }
+
+    /// <summary>
+    /// Public interface to thermal diffusivity model. Change the value of thermal diffusivity at x,y by incr
+    /// </summary>
+    /// <param name="x">x coord</param>
+    /// <param name="y">y coord</param>
+    /// <param name="incr">thermal diffusifity to set at x,y</param>
+    public void ChangeThermalDiffusivity(int x, int y, float incr)
+    {
+        if (IsWithinThermalDiffusivityBounds(thermalDiffusivity[GetIndex(x, y)] + incr)) {
+            thermalDiffusivity[GetIndex(x, y)] += incr;
+        }
+    }
+
+    /// <summary>
+    /// Checks wether temperature is admissible, if not warns you and returns false
+    /// </summary>
+    /// <param name="temp">Wanted temperature</param>
+    /// <returns>True if temperature is ok, false and a formal complaint if it's not ok</returns>
+    public bool IsWithinTemperatureBounds(float temp)
+    {
+        if (temp >= 0 && temp < Mathf.Infinity)
+        {
+            return true;
+        }
+        else
+        {
+            Debug.LogWarning(string.Format("Yep, something is wrong with your temperature: {0}.", temp));
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Checks wether thermal diffusivity is admissible, if not warns you and returns false
+    /// </summary>
+    /// <param name="thermal_diff">Wanted thermal diff.</param>
+    /// <returns>True if thermal diff. is ok, false and a formal complaint if it's not ok</returns>
+    public bool IsWithinThermalDiffusivityBounds(float thermal_diff)
+    {
+        if( thermal_diff >= 0 && thermal_diff < 1)
+        {
+            return true;
+        } else
+        {
+            Debug.LogWarning(string.Format("Trying to set a thermal diffusivity that may break the world: {0}.", thermal_diff));
+            return false;
+        }
+    }
+
+    ///// END OF PUBLIC INTERFACE
+    ///// PRIVATE STUFF
 
     /// <summary>
     /// Internal indexing of array
@@ -167,7 +262,7 @@ public class Temperature
         // TODO: Compute temperature sources
         // foreach(Tile tile in sinkAndSources)
         // GetTileAt(55, 55).temp_old = 1000;
-        if (sinksAndSources != null) sinksAndSources();
+        if (sinksAndSourcesActions != null) sinksAndSourcesActions();
 
         ForwardTemp();
     }
