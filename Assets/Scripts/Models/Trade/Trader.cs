868872b0cff92b0cd251049e2b1e982c93f5ed93
Ryan Shirk <rshirk@bradfordnetworks.com>
2016-08-22 10:20:26 -0400

    Revert "Merge remote-tracking branch 'refs/remotes/TeamPorcupine/master'"
    This reverts commit 261ef7482dba0de713f65fc2d3e026e7b9bad988, reversing
    changes made to 4c049a112cdff232a8f62bd997c497cdbd640557.

--- Assets/Scripts/Models/Trade/Trader.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-public class Trader
-{
-    public string Name;
-    public float CurrencyBalance;
-    public float SaleMarginMultiplier;
-    public List<Inventory> Stock;
-}
