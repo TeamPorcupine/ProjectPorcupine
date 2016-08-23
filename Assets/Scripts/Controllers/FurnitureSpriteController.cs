TeamPorcupine/ProjectPorcupine

commit 6626a78b0821c6b8a4c2171176a7d44c22a9f02a

Converted public fields to properties for Tile.cs (#545)
* Converted public fields to properties for Tile.cs

Converted all public fields in Tile.cs to properties and updated them to
use PascalCase naming convention. Left them with public get & set.

* Updated Furniture.lua to use new properties

* Fixed new errors that were merged in

Assets/Scripts/Controllers/FurnitureSpriteController.cs
@@ -68,8 +68,8 @@ public class FurnitureSpriteController
             Tile northTile = world.GetTileAt(furn.tile.X, furn.tile.Y + 1);
             Tile southTile = world.GetTileAt(furn.tile.X, furn.tile.Y - 1);
 
-            if (northTile != null && southTile != null && northTile.furniture != null && southTile.furniture != null &&
-            northTile.furniture.objectType.Contains("Wall") && southTile.furniture.objectType.Contains("Wall"))
+            if (northTile != null && southTile != null && northTile.Furniture != null && southTile.Furniture != null &&
+            northTile.Furniture.objectType.Contains("Wall") && southTile.Furniture.objectType.Contains("Wall"))
             {
                 furn_go.transform.rotation = Quaternion.Euler(0, 0, 90);
             }
@@ -156,13 +156,13 @@ public class FurnitureSpriteController
             Tile eastTile = world.GetTileAt(furn.tile.X + 1, furn.tile.Y);
             Tile westTile = world.GetTileAt(furn.tile.X - 1, furn.tile.Y);
 
-            if (northTile != null && southTile != null && northTile.furniture != null && southTile.furniture != null &&
-            northTile.furniture.objectType.Contains("Wall") && southTile.furniture.objectType.Contains("Wall"))
+            if (northTile != null && southTile != null && northTile.Furniture != null && southTile.Furniture != null &&
+            northTile.Furniture.objectType.Contains("Wall") && southTile.Furniture.objectType.Contains("Wall"))
             {
                 furn_go.transform.rotation = Quaternion.Euler(0, 0, 90);
             }
-            else if (eastTile != null && westTile != null && eastTile.furniture != null && westTile.furniture != null &&
-            eastTile.furniture.objectType.Contains("Wall") && westTile.furniture.objectType.Contains("Wall"))
+            else if (eastTile != null && westTile != null && eastTile.Furniture != null && westTile.Furniture != null &&
+            eastTile.Furniture.objectType.Contains("Wall") && westTile.Furniture.objectType.Contains("Wall"))
             {
                 furn_go.transform.rotation = Quaternion.Euler(0, 0, 0);
             }
@@ -274,22 +274,22 @@ public class FurnitureSpriteController
         Tile t;
 
         t = world.GetTileAt(x, y + 1);
-        if (t != null && t.furniture != null && t.furniture.objectType == furn.objectType)
+        if (t != null && t.Furniture != null && t.Furniture.objectType == furn.objectType)
         {
             spriteName += "N";
         }
         t = world.GetTileAt(x + 1, y);
-        if (t != null && t.furniture != null && t.furniture.objectType == furn.objectType)
+        if (t != null && t.Furniture != null && t.Furniture.objectType == furn.objectType)
         {
             spriteName += "E";
         }
         t = world.GetTileAt(x, y - 1);
-        if (t != null && t.furniture != null && t.furniture.objectType == furn.objectType)
+        if (t != null && t.Furniture != null && t.Furniture.objectType == furn.objectType)
         {
             spriteName += "S";
         }
         t = world.GetTileAt(x - 1, y);
-        if (t != null && t.furniture != null && t.furniture.objectType == furn.objectType)
+        if (t != null && t.Furniture != null && t.Furniture.objectType == furn.objectType)
         {
             spriteName += "W";
         }

Assets/Scripts/Controllers/BuildModeController.cs
@@ -101,9 +101,9 @@ public class BuildModeController
 
                 // Check if there is existing furniture in this tile. If so delete it.
                 // TODO Possibly return resources. Will the Deconstruct() method handle that? If so what will happen if resources drop ontop of new non-passable structure.
-                if (t.furniture != null)
+                if (t.Furniture != null)
                 {
-                    t.furniture.Deconstruct();
+                    t.Furniture.Deconstruct();
                 }
 
                 // Create a job for it to be build
@@ -131,11 +131,11 @@ public class BuildModeController
                     {
                         // FIXME: I don't like having to manually and explicitly set
                         // flags that preven conflicts. It's too easy to forget to set/clear them!
-                        Tile offsetTile = WorldController.Instance.world.GetTileAt(x_off, y_off);
-                        offsetTile.pendingBuildJob = j;
+                        Tile offsetTile = WorldController.Instance.world.GetTileAt(x_off,y_off);
+                        offsetTile.PendingBuildJob = j;
                         j.cbJobStopped += (theJob) =>
                             {
-                                offsetTile.pendingBuildJob = null;
+                                offsetTile.PendingBuildJob = null;
                             };
                     }
                 }
@@ -160,8 +160,8 @@ public class BuildModeController
 
             if ( 
                 t.Type != tileType && 
-                t.furniture == null &&
-                t.pendingBuildJob == null &&
+                t.Furniture == null &&
+                t.PendingBuildJob == null &&
                 CanBuildTileTypeHere(t, tileType))
             {
                 // This tile position is valid tile type
@@ -179,10 +179,10 @@ public class BuildModeController
 
                 // FIXME: I don't like having to manually and explicitly set
                 // flags that preven conflicts. It's too easy to forget to set/clear them!
-                t.pendingBuildJob = j;
+                t.PendingBuildJob = j;
                 j.cbJobStopped += (theJob) =>
                 {
-                    theJob.tile.pendingBuildJob = null;
+                    theJob.tile.PendingBuildJob = null;
                 };
 
                 // Add the job to the queue
@@ -199,13 +199,13 @@ public class BuildModeController
         else if (buildMode == BuildMode.DECONSTRUCT)
         {
             // TODO
-            if (t.furniture != null)
+            if (t.Furniture != null)
             {
-                t.furniture.Deconstruct();
+                t.Furniture.Deconstruct();
             }
-            else if (t.pendingBuildJob != null)
+            else if (t.PendingBuildJob != null)
             {
-                t.pendingBuildJob.CancelJob();
+                t.PendingBuildJob.CancelJob();
             }
         }
         else
@@ -224,7 +224,7 @@ public class BuildModeController
             case TileType.Floor:
                 return true;
             case TileType.Ladder:
-                return t.room.IsOutsideRoom();
+                return t.Room.IsOutsideRoom();
             default:
                 return true;
         }
@@ -236,7 +236,7 @@ public class BuildModeController
         {
             for (int y_off = t.Y; y_off < (t.Y + WorldController.Instance.world.furniturePrototypes[furnitureType].Height); y_off++)
             {
-                if (WorldController.Instance.world.GetTileAt(x_off, y_off).pendingBuildJob != null)
+                if (WorldController.Instance.world.GetTileAt(x_off, y_off).PendingBuildJob != null)
                 {
                     return true;
                 }

Assets/Scripts/Controllers/CharacterSpriteController.cs
@@ -111,13 +111,13 @@ public class CharacterSpriteController
         //Debug.Log(furn_go.GetComponent<SpriteRenderer>());
 
         //char_go.GetComponent<SpriteRenderer>().sprite = GetSpriteForFurniture(furn);
-        if (c.CurrTile.room != null)
+        if (c.CurrTile.Room != null)
         {
-            if (c.CurrTile.room.GetGasAmount ("O2") <= 0.5f && char_go.transform.GetChild(1).GetComponent<SpriteRenderer>().enabled == false)
+            if (c.CurrTile.Room.GetGasAmount ("O2") <= 0.5f && char_go.transform.GetChild(1).GetComponent<SpriteRenderer>().enabled == false)
             {
                 char_go.transform.GetChild(1).GetComponent<SpriteRenderer>().enabled = true;
             }
-            else if(c.CurrTile.room.GetGasAmount ("O2") >= 0.5f && char_go.transform.GetChild(1).GetComponent<SpriteRenderer>().enabled == true)
+            else if(c.CurrTile.Room.GetGasAmount ("O2") >= 0.5f && char_go.transform.GetChild(1).GetComponent<SpriteRenderer>().enabled == true)
             {
                 char_go.transform.GetChild(1).GetComponent<SpriteRenderer>().enabled = false;
             }

Assets/Scripts/Controllers/JobSpriteController.cs
@@ -89,8 +89,8 @@ public class JobSpriteController
             Tile northTile = world.GetTileAt(job.tile.X, job.tile.Y + 1);
             Tile southTile = world.GetTileAt(job.tile.X, job.tile.Y - 1);
 
-            if (northTile != null && southTile != null && northTile.furniture != null && southTile.furniture != null &&
-            northTile.furniture.objectType.Contains("Wall") && southTile.furniture.objectType.Contains("Wall"))
+            if (northTile != null && southTile != null && northTile.Furniture != null && southTile.Furniture != null &&
+            northTile.Furniture.objectType.Contains("Wall") && southTile.Furniture.objectType.Contains("Wall"))
             {
                 job_go.transform.rotation = Quaternion.Euler(0, 0, 90);
             }

Assets/Scripts/Controllers/MouseController.cs
@@ -177,10 +177,10 @@ public class MouseController
         if (Input.GetMouseButtonDown(1))
         {
             Tile tileUnderMouse = GetMouseOverTile();
-            if (tileUnderMouse.pendingBuildJob != null)
+            if (tileUnderMouse.PendingBuildJob != null)
             {
                 Debug.Log("Canceling!");
-                tileUnderMouse.pendingBuildJob.CancelJob();
+                tileUnderMouse.PendingBuildJob.CancelJob();
             }
         }
 
@@ -232,17 +232,17 @@ public class MouseController
     private void RebuildSelectionStuffInTile()
     {
         // Make sure stuffInTile is big enough to handle all the characters, plus the 3 extra values.
-        mySelection.stuffInTile = new ISelectable[mySelection.tile.characters.Count + 3];
+        mySelection.stuffInTile = new ISelectable[mySelection.tile.Characters.Count + 3];
 
         // Copy the character references.
-        for (int i = 0; i < mySelection.tile.characters.Count; i++)
+        for (int i = 0; i < mySelection.tile.Characters.Count; i++)
         {
-            mySelection.stuffInTile[i] = mySelection.tile.characters[i];
+            mySelection.stuffInTile[i] = mySelection.tile.Characters[i];
         }
 
         // Now assign references to the other three sub-selections available.
-        mySelection.stuffInTile[mySelection.stuffInTile.Length - 3] = mySelection.tile.furniture;
-        mySelection.stuffInTile[mySelection.stuffInTile.Length - 2] = mySelection.tile.inventory;
+        mySelection.stuffInTile[mySelection.stuffInTile.Length - 3] = mySelection.tile.Furniture;
+        mySelection.stuffInTile[mySelection.stuffInTile.Length - 2] = mySelection.tile.Inventory;
         mySelection.stuffInTile[mySelection.stuffInTile.Length - 1] = mySelection.tile;
     }
 

Assets/Scripts/Controllers/SpawnInventoryController.cs
@@ -100,12 +100,12 @@ public class SpawnInventoryController {
         Inventory inventoryChange = new Inventory(InventoryToBuild, 1);
 
         // You can't spawn on occupied tiles
-        if (t.furniture != null)
+        if (t.Furniture != null)
         {
             return; 
         }
 
-        if (t.inventory == null || t.inventory.objectType == InventoryToBuild)
+        if (t.Inventory == null || t.Inventory.objectType == InventoryToBuild)
         {
             World.current.inventoryManager.PlaceInventory(t, inventoryChange);
         }

Assets/Scripts/Models/Character.cs
@@ -72,11 +72,11 @@ public class Character : IXmlSerializable, ISelectable
         {
             if (_currTile != null)
             {
-                _currTile.characters.Remove(this);
+                _currTile.Characters.Remove(this);
             }
 
             _currTile = value;
-            _currTile.characters.Add(this);
+            _currTile.Characters.Add(this);
         }
     }
 
@@ -311,17 +311,18 @@ public class Character : IXmlSerializable, ISelectable
 
             // Are we standing on a tile with goods that are desired by the job?
             Debug.Log("Standing on Tile check");
-            if (CurrTile.inventory != null &&
-                myJob.DesiresInventoryType(CurrTile.inventory) > 0 && !CurrTile.inventory.isLocked &&
-                (myJob.canTakeFromStockpile || CurrTile.furniture == null || CurrTile.furniture.IsStockpile() == false))
+            if (CurrTile.Inventory != null &&
+                myJob.DesiresInventoryType(CurrTile.Inventory) > 0 && !CurrTile.Inventory.isLocked &&
+                (myJob.canTakeFromStockpile || CurrTile.Furniture == null || CurrTile.Furniture.IsStockpile() == false))
             {
                 // Pick up the stuff!
                 Debug.Log("Pick up the stuff");
 
                 World.current.inventoryManager.PlaceInventory(
                     this,
-                    CurrTile.inventory,
-                    myJob.DesiresInventoryType(CurrTile.inventory));
+                    CurrTile.Inventory,
+                    myJob.DesiresInventoryType(CurrTile.Inventory));
+                
             }
             else
             {
@@ -341,13 +342,11 @@ public class Character : IXmlSerializable, ISelectable
                 // Any chance we already have a path that leads to the items we want?
 
                 // Check that we have an end tile and that it has content.
-                if (pathAStar != null && pathAStar.EndTile() != null && pathAStar.EndTile().inventory != null &&
-                    
+                if (pathAStar != null && pathAStar.EndTile() != null && pathAStar.EndTile().Inventory != null &&
                     // Check if it is a stockpile and we are allowed to grab from it or just not a stockpile
-                    !(pathAStar.EndTile().furniture != null && (myJob.canTakeFromStockpile == false && pathAStar.EndTile().furniture.IsStockpile() == true)) &&
-
+                    !(pathAStar.EndTile().Furniture != null && (myJob.canTakeFromStockpile == false && pathAStar.EndTile().Furniture.IsStockpile() == true)) &&
                     // Check if contains the desired objectType
-                    (pathAStar.EndTile().inventory.objectType == desired.objectType))
+                    ( pathAStar.EndTile().Inventory.objectType == desired.objectType))
                 {
                     // We are already moving towards a tile that contains what we want!
                     // so....do nothing?
@@ -511,7 +510,7 @@ public class Character : IXmlSerializable, ISelectable
         }
 
         // How much distance can be travel this Update?
-        float distThisFrame = speed / NextTile.movementCost * deltaTime;
+        float distThisFrame = speed / NextTile.MovementCost * deltaTime;
 
         // How much is that in terms of percentage to our destination?
         float percThisFrame = distThisFrame / distToTravel;

Assets/Scripts/Models/Furniture.cs
@@ -317,9 +317,9 @@ public class Furniture : IXmlSerializable, ISelectable, IPowerRelated
                 for (int ypos = y - 1; ypos < (y + proto.Height + 1); ypos++)
                 {
                     t = World.current.GetTileAt(xpos, ypos);
-                    if (t != null && t.furniture != null && t.furniture.cbOnChanged != null)
+                    if (t != null && t.Furniture != null && t.Furniture.cbOnChanged != null)
                     {
-                        t.furniture.cbOnChanged(t.furniture);
+                        t.Furniture.cbOnChanged(t.Furniture);
                     }
                 }
             }
@@ -374,11 +374,11 @@ public class Furniture : IXmlSerializable, ISelectable, IPowerRelated
                 // Check to see if there is furniture which is replaceable
                 bool isReplaceable = false;
 
-                if (t2.furniture != null)
+                if (t2.Furniture != null)
                 {
                     for (int i = 0; i < ReplaceableFurniture.Count; i++)
                     {
-                        if (t2.furniture.HasTypeTag(ReplaceableFurniture[i]))
+                        if (t2.Furniture.HasTypeTag(ReplaceableFurniture[i]))
                         {
                             isReplaceable = true;
                         }
@@ -392,7 +392,7 @@ public class Furniture : IXmlSerializable, ISelectable, IPowerRelated
                 }
 
                 // Make sure tile doesn't already have furniture
-                if (t2.furniture != null && isReplaceable == false)
+                if (t2.Furniture != null && isReplaceable == false)
                 {
                     return false;
                 }
@@ -741,9 +741,9 @@ public class Furniture : IXmlSerializable, ISelectable, IPowerRelated
         int fwidth = 1;
         int fheight = 1;
         bool linksToNeighbour = false;
-        if (tile.furniture != null)
+        if (tile.Furniture != null)
         {
-            Furniture f = tile.furniture;
+            Furniture f = tile.Furniture;
             fwidth = f.Width;
             fheight = f.Height;
             linksToNeighbour = f.linksToNeighbour;
@@ -787,9 +787,9 @@ public class Furniture : IXmlSerializable, ISelectable, IPowerRelated
                 for (int ypos = y - 1; ypos < (y + fheight + 1); ypos++)
                 {
                     Tile t = World.current.GetTileAt(xpos, ypos);
-                    if (t != null && t.furniture != null && t.furniture.cbOnChanged != null)
+                    if (t != null && t.Furniture != null && t.Furniture.cbOnChanged != null)
                     {
-                        t.furniture.cbOnChanged(t.furniture);
+                        t.Furniture.cbOnChanged(t.Furniture);
                     }
                 }
             }

commit e1e0ea36015265e0c8b85897079fb9b3110da918

door rotation properly based on xml properties rather than hard coded (#644)
* all door types now rotate properly

* fixing name change breakages caused by PR #545

Assets/Scripts/Controllers/FurnitureSpriteController.cs
@@ -59,7 +59,7 @@ public class FurnitureSpriteController
         furn_go.transform.SetParent(furnnitureParent.transform, true);
 
         // FIXME: This hardcoding is not ideal!
-        if (furn.objectType == "Door" || furn.objectType == "Airlock")
+        if (furn.HasTypeTag("Door"))
         {
             // By default, the door graphic is meant for walls to the east & west
             // Check to see if we actually have a wall north/south, and if so
@@ -69,7 +69,7 @@ public class FurnitureSpriteController
             Tile southTile = world.GetTileAt(furn.tile.X, furn.tile.Y - 1);
 
             if (northTile != null && southTile != null && northTile.Furniture != null && southTile.Furniture != null &&
-            northTile.Furniture.objectType.Contains("Wall") && southTile.Furniture.objectType.Contains("Wall"))
+                northTile.Furniture.HasTypeTag("Wall") && southTile.Furniture.HasTypeTag("Wall"))
             {
                 furn_go.transform.rotation = Quaternion.Euler(0, 0, 90);
             }
@@ -144,8 +144,7 @@ public class FurnitureSpriteController
 
         GameObject furn_go = furnitureGameObjectMap[furn];
 
-        // FIXME: This hardcoding is not ideal!
-        if (furn.objectType == "Door" || furn.objectType == "Airlock")
+        if (furn.HasTypeTag("Door"))
         {
             // By default, the door graphic is meant for walls to the east & west
             // Check to see if we actually have a wall north/south, and if so
@@ -157,12 +156,12 @@ public class FurnitureSpriteController
             Tile westTile = world.GetTileAt(furn.tile.X - 1, furn.tile.Y);
 
             if (northTile != null && southTile != null && northTile.Furniture != null && southTile.Furniture != null &&
-            northTile.Furniture.objectType.Contains("Wall") && southTile.Furniture.objectType.Contains("Wall"))
+                northTile.Furniture.HasTypeTag("Wall") && southTile.Furniture.HasTypeTag("Wall"))
             {
                 furn_go.transform.rotation = Quaternion.Euler(0, 0, 90);
             }
             else if (eastTile != null && westTile != null && eastTile.Furniture != null && westTile.Furniture != null &&
-            eastTile.Furniture.objectType.Contains("Wall") && westTile.Furniture.objectType.Contains("Wall"))
+                eastTile.Furniture.HasTypeTag("Wall") && westTile.Furniture.HasTypeTag("Wall"))
             {
                 furn_go.transform.rotation = Quaternion.Euler(0, 0, 0);
             }