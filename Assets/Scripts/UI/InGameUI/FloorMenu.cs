3559fb5a90183c830d231bc5aad3cd913dba9d19
Julius Häger <julius_hager@hotmail.com>
2016-08-23 21:54:27 +0200

    Moddable tiles (#651)
    * Added moddable tiles.
    
    Also made the build tile menu generated programatically.
    
    * Added missing file
    
    * Fixed merge conflicts
    
    * Trying to fix conflicts
    
    * Added building jobs to tiles

--- Assets/Scripts/UI/InGameUI/FloorMenu.cs
+++ Assets/Scripts/UI/InGameUI/FloorMenu.cs
@@ -9,38 +9,77 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using ProjectPorcupine.Localization;
 
 public class FloorMenu : MonoBehaviour
 {
-    public Button floorBuild;
-    public Button ladderBuild;
-    public Button floorRemove;
+
+    public GameObject buildFurnitureButtonPrefab;
+
+    string lastLanguage;
+
+    // A chached copy to save gc. Could result in issues if TileType gets dynamically changed.
+    private TileType[] tileTypes;
+
+    //public Button floorBuild;
+    //public Button ladderBuild;
+    //public Button floorRemove;
 
     // Use this for initialization.
     void Start()
     {
+
+        tileTypes = TileType.GetTileTypes();
+
         BuildModeController bmc = WorldController.Instance.buildModeController;
 
-        floorBuild.onClick.AddListener(delegate
-            {
-                bmc.SetMode_BuildFloor();
-            });
-        if(ladderBuild != null)
+        // For each furniture prototype in our world, create one instance
+        // of the button to be clicked!
+        
+        foreach (TileType type in tileTypes)
         {
-            ladderBuild.onClick.AddListener(delegate
-                {
-                    bmc.SetMode_BuildLadder();
-                });
-        }
-        floorRemove.onClick.AddListener(delegate
+            GameObject go = (GameObject)Instantiate(buildFurnitureButtonPrefab);
+            go.transform.SetParent(this.transform);
+
+            TileType tileType = type;
+
+            go.name = "Button - Build " + tileType.Type;
+
+            // TODO: Not a elegant solution! Find a better way.
+            if (type == TileType.Empty)
             {
-                bmc.SetMode_Bulldoze();
+                go.name = "Button - Remove " + tileType.Type;
+
+                go.GetComponentInChildren<TextLocalizer>().defaultText = "remove";
+            }
+
+            go.transform.GetComponentInChildren<TextLocalizer>().formatValues = new string[] { LocalizationTable.GetLocalization(tileType.LocalizationCode) };
+
+            Button b = go.GetComponent<Button>();
+
+            b.onClick.AddListener(delegate
+            {
+                bmc.SetModeBuildTile(tileType);
             });
+
+        }
+
+        lastLanguage = LocalizationTable.currentLanguage;
     }
 
     // Update is called once per frame.
     void Update()
     {
+        if (lastLanguage != LocalizationTable.currentLanguage)
+        {
+            lastLanguage = LocalizationTable.currentLanguage;
 
+            TextLocalizer[] localizers = GetComponentsInChildren<TextLocalizer>();
+
+            for (int i = 0; i < localizers.Length; i++)
+            {
+                localizers[i].UpdateText(LocalizationTable.GetLocalization(tileTypes[i].LocalizationCode));
+            }
+        }
     }
 }
