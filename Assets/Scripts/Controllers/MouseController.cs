636944b9a5c86d3cef3b5f9dcbc8ae35891b4755
abackwood <a.backwood@gmail.com>
2016-08-23 12:39:55 +0200

    Refactored UpdateDragging()

--- Assets/Scripts/Controllers/MouseController.cs
+++ Assets/Scripts/Controllers/MouseController.cs
@@ -261,49 +261,72 @@ public class MouseController
             return;
         }
 
-        isDragging = CheckDragStart();
-        if(isDragging == false || bmc.IsObjectDraggable())
+        UpdateIsDragging();
+
+        if(isDragging == false || bmc.IsObjectDraggable() == false)
         {
+            Debug.Log("Not dragging");
             dragStartPosition = currPlacingPosition;
         }
 
-        if (Input.GetMouseButtonUp(1) || Input.GetKeyUp(KeyCode.Escape))
+        DragParameters dragParams = GetDragParameters();
+
+        ShowPreviews(dragParams);
+
+        // End Drag.
+        if (isDragging && Input.GetMouseButtonUp(0))
         {
-            // The RIGHT mouse button was released, so we
-            // are cancelling any dragging/build mode.
+            isDragging = false;
+
+            // If we're over a UI element, then bail out from this.
+            if (EventSystem.current.IsPointerOverGameObject())
+            {
+                return;
+            }
+
+            BuildOnDraggedTiles(dragParams);
+        }
+    }
+
+    private void CleanUpDragPreviews()
+    {
+        while (dragPreviewGameObjects.Count > 0)
+        {
+            GameObject go = dragPreviewGameObjects[0];
+            dragPreviewGameObjects.RemoveAt(0);
+            SimplePool.Despawn(go);
+        }
+    }
+
+    private void UpdateIsDragging()
+    {
+        // TODO Keyboard input does not belong in MouseController. Move to KeyboardController?
+        if (isDragging && (Input.GetMouseButtonUp(1) || Input.GetKeyDown(KeyCode.Escape)))
+        {
             isDragging = false;
         }
-
-        int start_x = Mathf.FloorToInt(dragStartPosition.x + 0.5f);
-        int end_x = Mathf.FloorToInt(currPlacingPosition.x + 0.5f);
-        int start_y = Mathf.FloorToInt(dragStartPosition.y + 0.5f);
-        int end_y = Mathf.FloorToInt(currPlacingPosition.y + 0.5f);
-
-        // Keep an un-flipped copy of start/end points
-        int raw_start_x = start_x;
-        int raw_end_x = end_x;
-        int raw_start_y = start_y;
-        int raw_end_y = end_y;
-
-        // We may be dragging in the "wrong" direction, so flip things if needed.
-        if (end_x < start_x)
+        else if(isDragging == false && Input.GetMouseButtonDown(0))
         {
-            int tmp = end_x;
-            end_x = start_x;
-            start_x = tmp;
+            isDragging = true;
         }
+    }
 
-        if (end_y < start_y)
-        {
-            int tmp = end_y;
-            end_y = start_y;
-            start_y = tmp;
-        }
+    private DragParameters GetDragParameters()
+    {
+        Debug.Log(dragStartPosition + ", " + currPlacingPosition);
 
-        // Display a preview of the drag area.
-        for (int x = start_x; x <= end_x; x++)
+        int startX = Mathf.FloorToInt(dragStartPosition.x + 0.5f);
+        int endX = Mathf.FloorToInt(currPlacingPosition.x + 0.5f);
+        int startY = Mathf.FloorToInt(dragStartPosition.y + 0.5f);
+        int endY = Mathf.FloorToInt(currPlacingPosition.y + 0.5f);
+        return new DragParameters(startX, endX, startY, endY);
+    }
+
+    private void ShowPreviews(DragParameters dragParams)
+    {
+        for (int x = dragParams.StartX; x <= dragParams.EndX; x++)
         {
-            for (int y = start_y; y <= end_y; y++)
+            for (int y = dragParams.StartY; y <= dragParams.EndY; y++)
             {
                 Tile t = WorldController.Instance.world.GetTileAt(x, y);
                 if (t != null)
@@ -312,43 +335,7 @@ public class MouseController
                     if (bmc.buildMode == BuildMode.FURNITURE)
                     {
                         Furniture proto = World.current.furniturePrototypes[bmc.buildModeObjectType];
-                        string dragType = proto.dragType;
-
-                        bool isValid = false;
-
-                        // Drag type validation.
-                        if (dragType == "border")
-                        {
-                            if (x == start_x || x == end_x || y == start_y || y == end_y)
-                            {
-                                isValid = true;
-                            } 
-                        }
-                        else if (dragType == "path")
-                        {
-                            bool xNeg = raw_start_x > raw_end_x ? true : false;
-
-                            if (xNeg && x >= raw_end_x && x <= raw_start_x)
-                            {
-                                if (y == raw_start_y || x == raw_end_x)
-                                {
-                                    isValid = true;
-                                }
-                            }
-                            else if (!xNeg && x <= raw_end_x && x >= raw_start_x)
-                            {
-                                if (y == raw_start_y || x == raw_end_x)
-                                {
-                                    isValid = true;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            isValid = true;
-                        }
-
-                        if (isValid)
+                        if(IsPartOfDrag(t, dragParams, proto.dragType))
                         {
                             ShowFurnitureSpriteAtTile(bmc.buildModeObjectType, t);
                         }
@@ -364,105 +351,52 @@ public class MouseController
                 }
             }
         }
+    }
 
-        // End Drag.
-        if (isDragging && Input.GetMouseButtonUp(0))
+    private void BuildOnDraggedTiles(DragParameters dragParams)
+    {
+        for (int x = dragParams.StartX; x <= dragParams.EndX; x++)
         {
-            isDragging = false;
-
-            // If we're over a UI element, then bail out from this.
-            if (EventSystem.current.IsPointerOverGameObject())
+            for (int y = dragParams.StartY; y <= dragParams.EndY; y++)
             {
-                return;
-            }
-
-            // Loop through all the tiles
-            for (int x = start_x; x <= end_x; x++)
-            {
-                for (int y = start_y; y <= end_y; y++)
+                Tile t = WorldController.Instance.world.GetTileAt(x, y);
+                if (bmc.buildMode == BuildMode.FURNITURE)
                 {
-                    Tile t = WorldController.Instance.world.GetTileAt(x, y);
-                    if (bmc.buildMode == BuildMode.FURNITURE)
+                    // Check for furniture dragType.
+                    Furniture proto = World.current.furniturePrototypes[bmc.buildModeObjectType];
+
+                    if (IsPartOfDrag(t, dragParams, proto.dragType))
                     {
-                        // Check for furniture dragType.
-                        Furniture proto = World.current.furniturePrototypes[bmc.buildModeObjectType];
-                        string dragType = proto.dragType;
-
-                        bool isValid = false;
-
-                        // Drag type validation.
-                        if (dragType == "border")
+                        if (t != null)
                         {
-                            if (x == start_x || x == end_x || y == start_y || y == end_y)
-                            {
-                                isValid = true;
-                            } 
-                        }
-                        else if (dragType == "path")
-                        {
-                            bool xNeg = raw_start_x > raw_end_x ? true : false;
-
-                            if (xNeg && x >= raw_end_x && x <= raw_start_x)
-                            {
-                                if (y == raw_start_y || x == raw_end_x)
-                                {
-                                    isValid = true;
-                                }
-                            }
-                            else if (!xNeg && x <= raw_end_x && x >= raw_start_x)
-                            {
-                                if (y == raw_start_y || x == raw_end_x)
-                                {
-                                    isValid = true;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            isValid = true;
-                        }
-
-                        if (isValid)
-                        {
-                            if (t != null)
-                            {
-                                // Call BuildModeController::DoBuild().
-                                bmc.DoBuild(t);
-                            }
+                            // Call BuildModeController::DoBuild().
+                            bmc.DoBuild(t);
                         }
                     }
-                    else
-                    {
-                        bmc.DoBuild(t);
-                    }
+                }
+                else
+                {
+                    bmc.DoBuild(t);
                 }
             }
         }
     }
 
-    private void CleanUpDragPreviews()
+    // Checks whether a tile is valid for the drag type, given the drag parameters
+    // Returns true if tile should be included, false otherwise
+    private bool IsPartOfDrag(Tile tile, DragParameters dragParams, string dragType)
     {
-        while (dragPreviewGameObjects.Count > 0)
+        switch(dragType)
         {
-            GameObject go = dragPreviewGameObjects[0];
-            dragPreviewGameObjects.RemoveAt(0);
-            SimplePool.Despawn(go);
+            case "border":
+                return tile.X == dragParams.StartX || tile.X == dragParams.EndX || tile.Y == dragParams.StartY || tile.Y == dragParams.EndY;
+            case "path":
+                bool withinXBounds = dragParams.StartX <= tile.X && tile.X <= dragParams.EndX;
+                bool onPath = tile.Y == dragParams.RawStartY || tile.X == dragParams.RawEndX;
+                return withinXBounds && onPath;
+            default:
+                return true;
         }
-    }
-
-    private bool CheckDragStart()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            dragStartPosition = currPlacingPosition;
-            return true;
-        }
-        return false;
-    }
-
-    private void CheckDragEnd()
-    {
-        
     }
 
     private void UpdateSpawnClicking()
@@ -548,6 +482,34 @@ public class MouseController
         Furniture proto = World.current.furniturePrototypes[furnitureType];
 
         go.transform.position = new Vector3(t.X + ((proto.Width - 1) / 2f), t.Y + ((proto.Height - 1) / 2f), 0);
+    }
+
+    public class DragParameters
+    {
+        public int RawStartX { get; private set; }
+        public int RawEndX { get; private set; }
+        public int RawStartY { get; private set; }
+        public int RawEndY { get; private set; }
+        public int StartX { get; private set; }
+        public int EndX { get; private set; }
+        public int StartY { get; private set; }
+        public int EndY { get; private set; }
+
+        public DragParameters(int startX, int endX, int startY, int endY)
+        {
+            this.RawStartX = startX;
+            this.RawEndX = endX;
+            this.RawStartY = startY;
+            this.RawEndY = endY;
+
+            this.StartX = Mathf.Min(startX, endX);
+            this.EndX = Mathf.Max(startX, endX);
+            this.StartY = Mathf.Min(startY, endY);
+            this.EndY = Mathf.Max(startY, endY);
+
+            Debug.Log("Drag params: (" + StartX + "," + StartY + ") - (" + EndX + "," + EndY + ")");
+
+        }
     }
 
     public class SelectionInfo
