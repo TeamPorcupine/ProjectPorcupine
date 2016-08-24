919dd0a853fa02c2eb414e4a6b296fbdaad93c5d
mikejbrown <michael.brown6@my.jcu.edu.au>
2016-08-25 00:05:08 +1000

    Implement UberLogger functions in ModUtils (#723)
    * Implement UberLogger functions in ModUtils
    
    * Logging functions with default 'Lua' channel
    
    * Removed test logging and updated lua script

--- Assets/Scripts/Utilities/ModUtils.cs
+++ Assets/Scripts/Utilities/ModUtils.cs
@@ -14,6 +14,8 @@ using UnityEngine;
 [MoonSharpUserData]
 public static class ModUtils
 {
+    static string defaultLogChannel = "Lua";
+
     public static float Clamp01(float value) 
     {
         return Mathf.Clamp01(value); 
@@ -32,5 +34,35 @@ public static class ModUtils
     public static void LogError(object obj) 
     {
         Debug.LogError(obj);
+    }
+
+    public static void ULogChannel(string channel, string message)
+    {
+        Debug.ULogChannel(channel, message);
+    }
+
+    public static void ULogWarningChannel(string channel, string message)
+    {
+        Debug.ULogWarningChannel(channel, message);
+    }
+
+    public static void ULogErrorChannel(string channel, string message)
+    {
+        Debug.ULogErrorChannel(channel, message);
+    }
+
+    public static void ULog(string message)
+    {
+        Debug.ULogChannel(defaultLogChannel, message);
+    }
+
+    public static void ULogWarning(string message)
+    {
+        Debug.ULogWarningChannel(defaultLogChannel, message);
+    }
+
+    public static void ULogError(string message)
+    {
+        Debug.ULogErrorChannel(defaultLogChannel, message);
     }
 }
