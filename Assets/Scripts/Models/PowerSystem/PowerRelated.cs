bd688016e8e9e8c58c92b41ce512981739c1893b
Grenkin <kruglovgleb@gmail.com>
2016-08-24 21:51:25 +0300

    Power system initial prototyping

---
+++ Assets/Scripts/Models/PowerSystem/PowerRelated.cs
@@ -0,0 +1,59 @@
+﻿#region License
+// ====================================================
+// Project Porcupine Copyright(C) 2016 Team Porcupine
+// This program comes with ABSOLUTELY NO WARRANTY; This is free software, 
+// and you are welcome to redistribute it under certain conditions; See 
+// file LICENSE, which is part of this source code package, for details.
+// ====================================================
+#endregion
+
+public class PowerRelated
+{
+    public float InputRate { get; private set; }
+
+    public float OutputRate { get; private set; }
+
+    public float Capacity { get; private set; }
+
+    public float AccumulatedPower { get; set; }
+
+    public bool IsEmpty
+    {
+        get
+        {
+            return AccumulatedPower.IsZero();
+        }
+    }
+
+    public bool IsFull
+    {
+        get
+        {
+            return AccumulatedPower.AreEqual(Capacity);
+        }
+    }
+
+    public bool IsPowerProducer
+    {
+        get
+        {
+            return InputRate.IsZero() && OutputRate > 0.0f;
+        }
+    }
+
+    public bool IsPowerConsumer
+    {
+        get
+        {
+            return OutputRate.IsZero() && InputRate > 0.0f;
+        }
+    }
+
+    public bool IsPowerAccumulator
+    {
+        get
+        {
+            return Capacity > 0.0f;
+        }
+    }
+}
