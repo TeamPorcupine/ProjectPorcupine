0e08b164f65e93d20ec88f583ec70ec3326b9d15
Oscar Sommerbo <o.sommerbo@gmail.com>
2016-08-26 22:55:06 +0200

    Switched to UberLogger. Removed old debug calls. (#849)

--- Assets/Scripts/Models/GameEventActions.cs
+++ Assets/Scripts/Models/GameEventActions.cs
@@ -50,7 +50,8 @@ public class GameEventActions
 
             if (func == null)
             {
-                Debug.LogError("'" + fn + "' is not a LUA function.");
+                //These errors are about the lua code so putting themin the lua channel
+                Debug.ULogErrorChannel("Lua", "'" + fn + "' is not a LUA function.");
                 return;
             }
 
@@ -58,7 +59,7 @@ public class GameEventActions
 
             if (result.Type == DataType.String)
             {
-                Debug.Log(result.String);
+                Debug.ULogErrorChannel("Lua", result.String);
             }
         }
     }
