b763189a497d8cb8bd8cec864c3994cbd8c0e654
Randshot <randshot@unknownrealm.eu>
2016-08-27 02:06:13 +0200

    StyleCop-ed Editor-Tests
    - /Assets/Editor/LivePowerSystemTest.cs
    - /Assets/Editor/ParametersEditorTest.cs
    - /Assets/Editor/PowerGridTest.cs

--- Assets/Editor/LivePowerSystemTest.cs
+++ Assets/Editor/LivePowerSystemTest.cs
@@ -6,7 +6,6 @@
 // file LICENSE, which is part of this source code package, for details.
 // ====================================================
 #endregion
-
 using System;
 using System.Collections.Generic;
 using System.Reflection;
