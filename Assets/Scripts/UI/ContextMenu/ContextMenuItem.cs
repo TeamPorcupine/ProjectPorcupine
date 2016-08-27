0a2a1354af4137487dc10577d2545dc3f3afb1c2
Tomás Malbrán <TomMalbran@users.noreply.github.com>
2016-08-27 00:13:04 -0300

    Fixed the code in the context menu files (#854)

--- Assets/Scripts/UI/ContextMenu/ContextMenuItem.cs
+++ Assets/Scripts/UI/ContextMenu/ContextMenuItem.cs
@@ -1,4 +1,12 @@
-﻿using UnityEngine;
+﻿#region License
+// ====================================================
+// Project Porcupine Copyright(C) 2016 Team Porcupine
+// This program comes with ABSOLUTELY NO WARRANTY; This is free software, 
+// and you are welcome to redistribute it under certain conditions; See 
+// file LICENSE, which is part of this source code package, for details.
+// ====================================================
+#endregion
+using UnityEngine;
 using UnityEngine.UI;
 
 public class ContextMenuItem : MonoBehaviour
@@ -13,11 +21,17 @@ public class ContextMenuItem : MonoBehaviour
         mouseController = WorldController.Instance.mouseController;
     }
 
+    /// <summary>
+    /// Builds the interface.
+    /// </summary>
     public void BuildInterface()
     {
         text.text = Action.Text;
     }
 
+    /// <summary>
+    /// Raises the click event.
+    /// </summary>
     public void OnClick()
     {
         if (Action != null)
