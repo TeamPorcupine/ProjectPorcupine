8e52f4c3a6ddd37edec7908553064015541704b8
Michael Brown <michael.brown6@my.jcu.edu.au>
2016-08-29 03:06:22 +1000

    Initial implementation of ScheduledEvent class

---
+++ Assets/Editor/ScheduledEventTest.cs
@@ -0,0 +1,155 @@
+﻿#region License
+// ====================================================
+// Project Porcupine Copyright(C) 2016 Team Porcupine
+// This program comes with ABSOLUTELY NO WARRANTY; This is free software, 
+// and you are welcome to redistribute it under certain conditions; See 
+// file LICENSE, which is part of this source code package, for details.
+// ====================================================
+#endregion
+using System;
+using NUnit.Framework;
+using Scheduler;
+using UnityEditor;
+using UnityEngine;
+
+public class ScheduledEventTest
+{
+    private Action<ScheduledEvent> callback;
+    private bool didRun = false;
+
+    [SetUp]
+    public void Init()
+    {
+        callback = (e) =>
+            {
+                Debug.ULogChannel("ScheduledEventTest", "Event {0} fired", e.Name);
+                didRun = true;
+            };
+    }
+
+    [Test]
+    public void EventCreationTest()
+    {
+        ScheduledEvent evt = new ScheduledEvent(
+            "test",
+            (e) => Debug.ULogChannel("ScheduledEventTest", "Event {0} fired", e.Name),
+            3.0f,
+            true,
+            1);
+
+        ScheduledEvent evt2 = new ScheduledEvent(evt);
+
+        Assert.That(evt.Name, Is.EqualTo("test"));
+        Assert.That(evt2.Name, Is.EqualTo("test"));
+        Assert.That(evt, Is.Not.EqualTo(evt2));
+    }
+
+    [Test]
+    public void EndlessEventRunTest()
+    {
+        // event that repeats forever
+        didRun = false;
+        ScheduledEvent evt = new ScheduledEvent(
+                                 "test1",
+                                 callback,
+                                 3.0f,
+                                 true,
+                                 1);
+        evt.Fire();
+        Assert.That(evt.LastShot, Is.False);
+        Assert.That(evt.Finished, Is.False);
+        Assert.That(didRun, Is.True);
+
+        // event that repeats forever -- confirm that repeats is ignored
+        didRun = false;
+        evt = new ScheduledEvent(
+            "test2",
+            callback,
+            3.0f,
+            true,
+            0);
+        evt.Fire();
+        Assert.That(evt.LastShot, Is.False);
+        Assert.That(evt.Finished, Is.False);
+        Assert.That(didRun, Is.True);
+    }
+
+    [Test]
+    public void NTimesRepeatedEventRunTest()
+    {
+        // event that repeats twice
+        didRun = false;
+        ScheduledEvent evt = new ScheduledEvent(
+            "test3",
+            callback,
+            3.0f,
+            false,
+            2);
+        Assert.That(evt.LastShot, Is.False);
+        Assert.That(evt.Finished, Is.False);
+
+        evt.Fire();
+        Assert.That(didRun, Is.True);
+        Assert.That(evt.LastShot, Is.True);
+        Assert.That(evt.Finished, Is.False);
+        didRun = false;
+
+        evt.Fire();
+        Assert.That(didRun, Is.True);
+        Assert.That(evt.LastShot, Is.False);
+        Assert.That(evt.Finished, Is.True);
+
+        didRun = false;
+        evt.Fire();
+        Assert.That(evt.LastShot, Is.False);
+        Assert.That(didRun, Is.False);
+    }
+
+    [Test]
+    public void CooldownEventRunTest()
+    {
+        ScheduledEvent evt = new ScheduledEvent(
+            "test4",
+            callback,
+            3.0f,
+            true,
+            1);
+
+        // doesn't run until the cooldown is reached
+        didRun = false;
+        evt.Update(2.0f);
+        Assert.That(evt.LastShot, Is.False);
+        Assert.That(evt.Finished, Is.False);
+        Assert.That(evt.TimeToWait, Is.EqualTo(1.0f));
+        Assert.That(didRun, Is.False);
+
+        didRun = false;
+        evt.Update(1.0f);
+        Assert.That(evt.LastShot, Is.False);
+        Assert.That(evt.Finished, Is.False);
+        Assert.That(didRun, Is.True);
+
+        // timer should be reset after firing
+        Assert.That(evt.TimeToWait, Is.EqualTo(evt.Cooldown));
+
+        // so it should work again as above
+        didRun = false;
+        evt.Update(2.0f);
+        Assert.That(evt.LastShot, Is.False);
+        Assert.That(evt.Finished, Is.False);
+        Assert.That(didRun, Is.False);
+
+        didRun = false;
+        evt.Update(1.0f);
+        Assert.That(evt.LastShot, Is.False);
+        Assert.That(evt.Finished, Is.False);
+        Assert.That(didRun, Is.True);
+
+        // and it should also work if we overshoot the cooldown
+        didRun = false;
+        evt.Update(5.0f);
+        Assert.That(evt.LastShot, Is.False);
+        Assert.That(evt.Finished, Is.False);
+        Assert.That(didRun, Is.True);
+    }
+}
---
+++ Assets/Scripts/Scheduler/ScheduledEvent.cs
@@ -0,0 +1,140 @@
+﻿#region License
+// ====================================================
+// Project Porcupine Copyright(C) 2016 Team Porcupine
+// This program comes with ABSOLUTELY NO WARRANTY; This is free software,
+// and you are welcome to redistribute it under certain conditions; See
+// file LICENSE, which is part of this source code package, for details.
+// ====================================================
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace Scheduler
+{
+    /// <summary>
+    /// The <see cref="Scheduler.ScheduledEvent"/> class represents an individual event which is handled by the Scheduler.
+    /// May "fire" either a C# or Lua function.
+    /// </summary>
+    public class ScheduledEvent : IXmlSerializable
+    {
+        // TODO: Lua event handling!
+        // TODO: Serialization!
+        private string luaFunctionName; // not used for anything yet
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Scheduler.ScheduledEvent"/> class.
+        /// </summary>
+        /// <param name="name">Name of the event (for serialization etc.).</param>
+        /// <param name="onFire">Callback to call when the event fires.</param>
+        /// <param name="cooldown">Cooldown in seconds.</param>
+        /// <param name="repeatsForever">Whether the event repeats forever (defaults false). If true repeats is ignored.</param>
+        /// <param name="repeats">Number of repeats (default 1). Ignored if repeatsForever=true.</param>
+        public ScheduledEvent(string name, Action<ScheduledEvent> onFire, float cooldown, bool repeatsForever = false, int repeats = 1)
+        {
+            this.Name = name;
+            this.OnFire = onFire;
+            this.Cooldown = cooldown;
+            this.TimeToWait = cooldown;
+            this.RepeatsForever = repeatsForever;
+            this.RepeatsLeft = repeats;
+        }
+
+        public ScheduledEvent(ScheduledEvent other)
+        {
+            this.Name = other.Name;
+            this.OnFire = other.OnFire;
+            this.Cooldown = other.Cooldown;
+            this.TimeToWait = other.Cooldown;
+            this.RepeatsForever = other.RepeatsForever;
+            this.RepeatsLeft = other.RepeatsLeft;
+        }
+
+        private event Action<ScheduledEvent> OnFire;
+
+        public string Name { get; protected set; }
+
+        public int RepeatsLeft { get; protected set; }
+
+        public float Cooldown { get; protected set; }
+
+        public float TimeToWait { get; protected set; }
+
+        public bool RepeatsForever { get; protected set; }
+
+        public bool LastShot
+        {
+            get
+            {
+                return RepeatsLeft == 1 && RepeatsForever == false;
+            }
+        }
+
+        public bool Finished
+        {
+            get
+            {
+                return RepeatsLeft < 1 && RepeatsForever == false;
+            }
+        }
+
+        /// <summary>
+        /// Advance the event clock by the specified deltaTime, and if it drops less that or equal to zero fire the event, resetting the clock to Cooldown.
+        /// </summary>
+        /// <param name="deltaTime">Delta time in seconds (note: game time, not real time).</param>
+        public void Update(float deltaTime)
+        {
+            this.TimeToWait -= deltaTime;
+
+            if (this.TimeToWait <= 0)
+            {
+                Fire();
+                this.TimeToWait = this.Cooldown;
+            }
+        }
+
+        public void Fire()
+        {
+            if (RepeatsLeft < 1 && RepeatsForever == false)
+            {
+                Debug.ULogChannel("ScheduledEvent", "Scheduled event '" + Name + "' finished last repeat.");
+                return;
+            }
+
+            if (this.OnFire != null)
+            {
+                this.OnFire(this);
+            }
+
+            this.RepeatsLeft -= 1;
+        }
+
+        public void Stop()
+        {
+            RepeatsLeft = 0;
+            RepeatsForever = false;
+        }
+
+        #region IXmlSerializable implementation
+
+        public XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
