fc3c80dd32073ee6938b568f00de6639bc7f987e
janpeli <janpeli@gmail.com>
2016-08-30 20:34:58 +0200

    Added Time Controller and cleaned up references in keyboardcontroller and world controller

--- Assets/Scripts/Controllers/WorldController.cs
+++ Assets/Scripts/Controllers/WorldController.cs
@@ -31,8 +31,8 @@ public class WorldController : MonoBehaviour
     public KeyboardController keyboardController;
     public CameraController cameraController;
     public SpawnInventoryController spawnInventoryController;
+    public TimeController timeController;
     public ModsManager modsManager;
-    public float GameTickPerSecond = 5;
     public GameObject inventoryUI;
     public GameObject circleCursorPrefab;
 
@@ -42,11 +42,7 @@ public class WorldController : MonoBehaviour
     private static string loadWorldFromFile = null;
 
     private float gameTickDelay;
-    private float totalDeltaTime;
     private bool isPaused = false;
-
-    // Multiplier of Time.deltaTime.
-    private float timeScale = 1f;
 
     public static WorldController Instance { get; protected set; }
 
@@ -70,7 +66,7 @@ public class WorldController : MonoBehaviour
     {
         get
         {
-            return timeScale;
+            return timeController.TimeScale;
         }
     }
     
@@ -102,7 +98,7 @@ public class WorldController : MonoBehaviour
 
         soundController = new SoundController(World);
 
-        gameTickDelay = 1f / GameTickPerSecond;
+        gameTickDelay = timeController.GameTickDelay;
     }
 
     public void Start()
@@ -123,6 +119,7 @@ public class WorldController : MonoBehaviour
         keyboardController = new KeyboardController(buildModeController, Instance);
         questController = new QuestController();
         cameraController = new CameraController();
+        timeController = new TimeController();
 
         // Hiding Dev Mode spawn inventory controller if devmode is off.
         spawnInventoryController.SetUIVisibility(Settings.GetSettingAsBool("DialogBoxSettings_developerModeToggle", false));
@@ -142,41 +139,28 @@ public class WorldController : MonoBehaviour
         mouseController.Update(IsModal);
         keyboardController.Update(IsModal);
         cameraController.Update(IsModal);
-
-        float deltaTime = Time.deltaTime * timeScale;
+        timeController.Update();
 
         // Systems that update every frame when not paused.
         if (IsPaused == false)
         {
-            World.UpdateCharacters(deltaTime);
+            World.UpdateCharacters(timeController.DeltaTime);
         }
 
-        totalDeltaTime += deltaTime;
-
-        if (totalDeltaTime >= gameTickDelay)
+        if (timeController.TotalDeltaTime >= gameTickDelay)
         {
             // Systems that update at fixed frequency. 
             if (IsPaused == false)
             {
                 // Systems that update at fixed frequency when not paused.
-                World.Tick(totalDeltaTime);
-                questController.Update(totalDeltaTime);
+                World.Tick(timeController.TotalDeltaTime);
+                questController.Update(timeController.TotalDeltaTime);
             }
 
-            totalDeltaTime = 0f;
+            timeController.ResetTotalDeltaTime();
         }
 
         soundController.Update(Time.deltaTime);
-    }
-
-    /// <summary>
-    /// Set's game speed (it's a multiplier so 1 == normal game speed).
-    /// </summary>
-    /// <param name="timeScale">Desired time scale.</param>
-    public void SetTimeScale(float timeScale)
-    {
-        this.timeScale = timeScale;
-        Debug.ULogChannel("Game speed", "Game speed set to " + timeScale + "x");
     }
 
     /// <summary>
