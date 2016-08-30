TeamPorcupine/ProjectPorcupine

commit cf31cffd65f643ee05ed21a64b4901bbdd12b326

Trade ship visit (#817)
* Auto font license edition

* Trade ship visit

* Trade is working (but without currency for now) : Give item from stockpiles and item bougth spaw near landing pad

* Trader now use currencies from #816

* Remove old LandingPad Test Action & Behaviors

* Revert change in Settings.xml

* Oups - reverted this time

* Fix many stylecop issues, only 5 remaining in the whole solution, but I don't understand them

========== Violation Count: 5 ==========

* Replacing many var by specific types

* Remove more var (no more left in Solution beside UberLoger & Priority Queue - not part of this PR)

Assets/Scripts/Controllers/WorldController.cs
@@ -11,10 +11,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml.Serialization;
+using MoonSharp.Interpreter;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
+[MoonSharpUserData]
 public class WorldController : MonoBehaviour
 {
     public SoundController soundController;
@@ -37,15 +39,6 @@ public class WorldController : MonoBehaviour
     // If true, a modal dialog box is open so normal inputs should be ignored.
     public bool IsModal;
 
-    private static string loadWorldFromFile = null;
-
-    private float gameTickDelay;
-    private float totalDeltaTime;
-    private bool isPaused = false;
-
-    // Multiplier of Time.deltaTime.
-    private float timeScale = 1f;
-
     public static WorldController Instance { get; protected set; }
 
     // The world and tile data.
@@ -63,6 +56,23 @@ public class WorldController : MonoBehaviour
             isPaused = value;
         }
     }
+
+    public float TimeScale
+    {
+        get
+        {
+            return timeScale;
+        }
+    }
+
+    private static string loadWorldFromFile = null;
+
+    private float gameTickDelay;
+    private float totalDeltaTime;
+    private bool isPaused = false;
+
+    // Multiplier of Time.deltaTime.
+    private float timeScale = 1f;
 
     // Use this for initialization.
     public void OnEnable()
@@ -201,6 +211,26 @@ public class WorldController : MonoBehaviour
         // Reload the scene to reset all data (and purge old references)
         loadWorldFromFile = fileName;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void CallTradeShipTest(Furniture landingPad)
+    {
+        // Currently not using any logic to select a trader
+        TraderPrototype prototype = PrototypeManager.Trader.GetPrototype(Random.Range(0, PrototypeManager.Trader.Count - 1));
+        Trader trader = prototype.CreateTrader();
+
+        GameObject go = new GameObject(trader.Name);
+        go.transform.parent = transform;
+        TraderShipController controller = go.AddComponent<TraderShipController>();
+        controller.Trader = trader;
+        controller.Speed = 5f;
+        go.transform.position = new Vector3(-10, 50, 0);
+        controller.LandingCoordinates = new Vector3(landingPad.Tile.X + 1, landingPad.Tile.Y + 1, 0);
+        controller.LeavingCoordinates = new Vector3(100, 50, 0);
+        go.transform.localScale = new Vector3(1, 1, 1);
+        SpriteRenderer spriteRenderer = go.AddComponent<SpriteRenderer>();
+        spriteRenderer.sprite = SpriteManager.current.GetSprite("Trader", "BasicHaulShip");
+        spriteRenderer.sortingLayerName = "TradeShip";
     }
 
     private void CreateEmptyWorld()

Assets/Scripts/Controllers/TraderShipController.cs
@@ -1,0 +1,99 @@
+﻿#region License
+// ====================================================
+// Project Porcupine Copyright(C) 2016 Team Porcupine
+// This program comes with ABSOLUTELY NO WARRANTY; This is free software, 
+// and you are welcome to redistribute it under certain conditions; See 
+// file LICENSE, which is part of this source code package, for details.
+// ====================================================
+#endregion
+using UnityEngine;
+
+public class TraderShipController : MonoBehaviour
+{
+    public Vector3 LeavingCoordinates;
+    public Vector3 LandingCoordinates;
+    public float Speed;
+    public float DestinationReachedThreshold = 0.1f;
+    public bool DestinationReached;
+    public bool TradeCompleted;
+    public Trader Trader;
+
+    public void FixedUpdate()
+    {
+        if (WorldController.Instance.IsPaused)
+        {
+            return;
+        }
+
+        Vector3 destination = LandingCoordinates;
+
+        if (DestinationReached && !TradeCompleted)
+        {
+            return;
+        }
+
+        if (TradeCompleted)
+        {
+            destination = LeavingCoordinates;
+        }
+
+        float distance = Vector3.Distance(transform.position, destination);
+
+        if (distance > DestinationReachedThreshold)
+        {
+            // rotate the model
+            Vector3 vectorToTarget = destination - transform.position;
+            float angle = (Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg) - 90;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * Speed * WorldController.Instance.TimeScale);
+
+            // Direction to the next waypoint
+            Vector3 dir = (destination - transform.position).normalized;
+            dir *= Speed * Time.fixedDeltaTime * WorldController.Instance.TimeScale;
+
+            transform.position = transform.position + dir;
+        }
+        else
+        {
+            DestinationReached = true;
+            if (TradeCompleted)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                DialogBoxManager dbm = GameObject.Find("Dialog Boxes").GetComponent<DialogBoxManager>();
+
+                Trader playerTrader = Trader.FromPlayer(World.Current.Wallet.Currencies[Trader.Currency.Name]);
+                Trade trade = new Trade(playerTrader, Trader);
+                dbm.dialogBoxTrade.SetupTrade(trade);
+                dbm.dialogBoxTrade.OnTradeCancelled = () => { TradeCompleted = true; };
+                dbm.dialogBoxTrade.OnTradeCompleted = () =>
+                {
+                    TradeCompleted = true;
+                    TrasfertTradedItems(trade);
+                };
+                dbm.dialogBoxTrade.ShowDialog();
+            }
+        }
+    }
+
+    private void TrasfertTradedItems(Trade trade)
+    {
+        trade.Player.Currency.Balance += trade.TradeCurrencyBalanceForPlayer;
+
+        foreach (TradeItem tradeItem in trade.TradeItems)
+        {
+            if (tradeItem.TradeAmount > 0)
+            {
+                Tile tile = WorldController.Instance.World.GetFirstTileWithNoInventoryAround(6, (int)LandingCoordinates.x, (int)LandingCoordinates.y);
+                Inventory inv = new Inventory(tradeItem.ObjectType, tradeItem.TradeAmount, tradeItem.TradeAmount);
+                WorldController.Instance.World.inventoryManager.PlaceInventory(tile, inv);
+            }
+            else if (tradeItem.TradeAmount < 0)
+            {
+                WorldController.Instance.World.inventoryManager.QuickRemove(tradeItem.ObjectType, -tradeItem.TradeAmount, true);
+            }
+        }
+    }
+}

Assets/Scripts/Controllers/TraderShipController.cs.meta
@@ -1,0 +1,12 @@
+fileFormatVersion: 2
+guid: 3c9cd89aa8fd55a459514fa519fe396c
+timeCreated: 1472172119
+licenseType: Pro
+MonoImporter:
+  serializedVersion: 2
+  defaultReferences: []
+  executionOrder: 0
+  icon: {instanceID: 0}
+  userData: 
+  assetBundleName: 
+  assetBundleVariant: 

Assets/Scripts/Models/FurnitureActions.cs
@@ -23,6 +23,7 @@ public class FurnitureActions
         LuaUtilities.RegisterGlobal(typeof(Job));
         LuaUtilities.RegisterGlobal(typeof(ModUtils));
         LuaUtilities.RegisterGlobal(typeof(World));
+        LuaUtilities.RegisterGlobal(typeof(WorldController));
         LuaUtilities.RegisterGlobal(typeof(Power.Connection));
 
         LoadScripts();

Assets/Scripts/Models/InventoryManager.cs
@@ -6,7 +6,10 @@
 // file LICENSE, which is part of this source code package, for details.
 // ====================================================
 #endregion
+
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using MoonSharp.Interpreter;
 using UnityEngine;
 
@@ -147,6 +150,37 @@ public class InventoryManager
         }
 
         return true;
+    }
+
+    public bool QuickRemove(string objectType, int quantity, bool onlyFromStockpiles)
+    {
+        if (QuickCheck(objectType))
+        {
+            foreach (Inventory inventory in inventories[objectType].ToList())
+            {
+                if (onlyFromStockpiles)
+                {
+                    if (inventory.tile == null || 
+                        inventory.tile.Furniture == null ||
+                        inventory.tile.Furniture.ObjectType != "Stockpile")
+                    {
+                        continue;
+                    }
+                }
+
+                if (quantity <= 0)
+                {
+                    break;
+                }
+
+                int removedFromStack = Math.Min(inventory.StackSize, quantity);
+                quantity -= removedFromStack;
+                inventory.StackSize -= removedFromStack;
+                CleanupInventory(inventory);
+            }
+        }
+
+        return quantity == 0;
     }
 
     public Path_AStar GetPathToClosestInventoryOfType(string objectType, Tile t, int desiredAmount, bool canTakeFromStockpile)

Assets/Scripts/Models/Trade/Trade.cs
@@ -21,7 +21,7 @@ public class Trade
         Player = player;
         Trader = trader;
 
-        var totalStock = new List<Inventory>();
+        List<Inventory> totalStock = new List<Inventory>();
         totalStock.AddRange(player.Stock);
         totalStock.AddRange(trader.Stock);
         TradeItems = totalStock.GroupBy(s => s.objectType).Select(g => new TradeItem
@@ -44,13 +44,8 @@ public class Trade
         }
     }
 
-    public void Accept()
-    {
-        // TODO
-    }
-
     public bool IsValid()
     {
-        return true; // TODO
+        return Player.Currency.Balance > TradeCurrencyBalanceForPlayer;
     }
 }

Assets/Scripts/Models/Trade/Trader.cs
@@ -7,12 +7,41 @@
 // ====================================================
 #endregion
 using System.Collections.Generic;
+using System.Linq;
 
 public class Trader
 {
     public string Name;
-    public string CurrencyName;
-    public float CurrencyBalance;
+    public Currency Currency;
     public float SaleMarginMultiplier;
     public List<Inventory> Stock;
+
+    public static Trader FromPlayer(Currency currency)
+    {
+        Trader t = new Trader
+        {
+            Name = "Player",
+            SaleMarginMultiplier = 0.8f,
+            Stock = new List<Inventory>(),
+            Currency = currency
+        };
+
+        List<List<Inventory>> worldInventories =
+            WorldController.Instance.World.inventoryManager.inventories.Values.Select(i => i.ToList()).ToList();
+        
+        foreach (List<Inventory> worldInventory in worldInventories)
+        {
+            foreach (Inventory inventory in worldInventory)
+            {
+                if (inventory.tile != null && 
+                    inventory.tile.Furniture != null &&
+                    inventory.tile.Furniture.ObjectType == "Stockpile")
+                {
+                    t.Stock.Add(inventory);
+                }
+            }
+        }
+        
+        return t;
+    }
 }

Assets/Scripts/Models/Trade/TraderPrototype.cs
@@ -96,15 +96,20 @@ public class TraderPrototype
     {
         Trader t = new Trader
         {
-            CurrencyName = CurrencyName,
-            CurrencyBalance = Random.Range(MinCurrencyBalance, MaxCurrencyBalance),
-            Name = PotentialNames[Random.Range(PotentialNames.Count, PotentialNames.Count - 1)],
-            SaleMarginMultiplier = Random.Range(MinSaleMarginMultiplier, MaxSaleMarginMultiplier)
+            Currency = new Currency
+            {
+                Name = CurrencyName,
+                Balance = Random.Range(MinCurrencyBalance, MaxCurrencyBalance),   
+                ShortName = World.Current.Wallet.Currencies[CurrencyName].ShortName
+            },
+            Name = PotentialNames[Random.Range(0, PotentialNames.Count - 1)],
+            SaleMarginMultiplier = Random.Range(MinSaleMarginMultiplier, MaxSaleMarginMultiplier),
+            Stock = new List<Inventory>()
         };
 
-        foreach (var potentialStock in PotentialStock)
+        foreach (TraderPotentialInventory potentialStock in PotentialStock)
         {
-            var itemIsInStock = Random.Range(0f, 1f) > potentialStock.Rarity;
+            bool itemIsInStock = Random.Range(0f, 1f) > potentialStock.Rarity;
 
             if (itemIsInStock)
             {

commit 282567ec0795b9ae1c2a76abd2e6ea0a0864ecdd

Fixed StyleCop issues introduced in #817 Was pretty easy to do.

Assets/Scripts/Controllers/WorldController.cs
@@ -39,6 +39,15 @@ public class WorldController : MonoBehaviour
     // If true, a modal dialog box is open so normal inputs should be ignored.
     public bool IsModal;
 
+    private static string loadWorldFromFile = null;
+
+    private float gameTickDelay;
+    private float totalDeltaTime;
+    private bool isPaused = false;
+
+    // Multiplier of Time.deltaTime.
+    private float timeScale = 1f;
+
     public static WorldController Instance { get; protected set; }
 
     // The world and tile data.
@@ -64,16 +73,7 @@ public class WorldController : MonoBehaviour
             return timeScale;
         }
     }
-
-    private static string loadWorldFromFile = null;
-
-    private float gameTickDelay;
-    private float totalDeltaTime;
-    private bool isPaused = false;
-
-    // Multiplier of Time.deltaTime.
-    private float timeScale = 1f;
-
+    
     // Use this for initialization.
     public void OnEnable()
     {