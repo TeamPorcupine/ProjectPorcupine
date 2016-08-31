21eef2531925d13b9de11913cfeebbd1ea5b88ab
Ferr013 <g.ferrami@gmail.com>
2016-08-31 13:40:18 +0200

    MusicController now streams properly from StreamingAssets

--- Assets/Scripts/Controllers/MusicController.cs
+++ Assets/Scripts/Controllers/MusicController.cs
@@ -8,20 +8,57 @@
 #endregion
 
 using UnityEngine;
-using System.Collections;
+using System;
+using System.IO;
+using NVorbis;
 
-[RequireComponent (typeof (AudioSource))]
+[RequireComponent(typeof(AudioSource))]
 public class MusicController : MonoBehaviour
 {
+    MemoryStream stream;
+    VorbisReader vorbis;
     AudioSource backgroundMusic;
-// FIXME: Need to load the music file from StreamingAssets instead of Resources
-    void Awake ()
+    public string musicTitle = "SpaceTheme";
+
+    void OnEnable()
     {
         backgroundMusic = GetComponent<AudioSource>();
-        AudioClip ac = Resources.Load<AudioClip>("Music/SpaceTheme");
+        AudioSettings.outputSampleRate = 44100;
+        AudioClip ac = LoadMusic(musicTitle);
         backgroundMusic.clip = ac;
         backgroundMusic.loop = true;
         backgroundMusic.Play();
 
-	}
+    }
+
+    private AudioClip LoadMusic(string title)
+    {
+        string filePath = Application.streamingAssetsPath;
+        filePath = Path.Combine(filePath, "Music");
+        filePath = Path.Combine(filePath, title + ".ogg");
+
+        byte[] dataAsByteArray = File.ReadAllBytes(filePath);
+        stream = new MemoryStream(dataAsByteArray);
+        vorbis = new VorbisReader(stream, true);
+
+        int samplecount = (int)(vorbis.SampleRate * vorbis.TotalTime.TotalSeconds);
+
+        return AudioClip.Create(title, samplecount, vorbis.Channels, vorbis.SampleRate, false, true, OnAudioRead, OnAudioSetPosition);
+    }
+
+    void OnAudioRead(float[] data)
+    {
+        var f = new float[data.Length];
+        vorbis.ReadSamples(f, 0, data.Length);
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            data[i] = f[i];
+        }
+    }
+
+    void OnAudioSetPosition(int newPosition)
+    {
+        vorbis.DecodedTime = new TimeSpan(0);
+    }
 }
