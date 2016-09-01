db8d1389439f987179926186ff0fbe984645cdf3
Brian <brian2386@gmail.com>
2016-08-31 22:24:34 -0400

    log results

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -8,12 +8,12 @@ echo "Attempting Unit Tests"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -runEditorTests \
-  -logFile $(pwd)/unity.log \
+  -editorTestsResultFile $(pwd)/unity.log \
   -projectPath $(pwd) \
   -quit
 
 
-echo 'Logs from Tests'
+echo 'Results from Tests'
 cat $(pwd)/unity.log
 #can't do windows and linux builds because unity by default installs only with build module
 #for the platform your on. 
