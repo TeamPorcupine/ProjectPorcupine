c070c26729c700c908b7af31228b23ad77596153
Brian <brian2386@gmail.com>
2016-08-31 22:12:02 -0400

    removed osx build step

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,20 +3,6 @@
 # Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
 
 # Change this the name of your project. This will be the name of the final executables as well.
-project="project-porcupine"
-
-echo " to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
-  -quit
-
-echo 'Logs from build'
-cat $(pwd)/unity.log
 
 echo "Attempting Unit Tests"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
