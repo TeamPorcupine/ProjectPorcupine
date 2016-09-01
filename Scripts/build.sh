34a6198670fa88819c51011731391f4655045e20
Bjubes <brian2386@gmail.com>
2016-09-01 17:16:37 -0400

    forgot to save

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,13 +3,20 @@
 # Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
 
 # Change this the name of your project. This will be the name of the final executables as well.
+project="project-porcupine"
 
-echo "Attempting Unit Tests"
+echo "Attempting to build $project for OS X"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+  -quit
 
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -runEditorTests -projectPath $(pwd) -logFile unity.log 
+echo 'Logs from build'
 cat $(pwd)/unity.log
-ls
-echo 'Results from Tests'
-cat $(pwd)/EditorTestResults.xml
+
 #can't do windows and linux builds because unity by default installs only with build module
 #for the platform your on. 
