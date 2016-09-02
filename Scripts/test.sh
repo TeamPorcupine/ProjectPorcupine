3288b40a4a397b690815b7f6e853ff7ee6fa3292
Brian <brian2386@gmail.com>
2016-09-02 16:14:00 -0400

    updated comments

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -1,9 +1,5 @@
 #! /bin/sh
 
-# Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
-
-# Change this the name of your project. This will be the name of the final executables as well.
-
 echo "Attempting Unit Tests"
 echo 'travis_fold:start:compile'
 /Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -runEditorTests -nographics -EditorTestResultFile $(pwd)/EditorTestResults.xml -projectPath $(pwd) -logFile unity.log 
