d7c76215bc78160874991ef8b0cb1f256835849e
abackwood <a.backwood@gmail.com>
2016-09-04 10:18:42 +0200

    Style cop

--- Assets/Scripts/Controllers/ShipSpriteController.cs
+++ Assets/Scripts/Controllers/ShipSpriteController.cs
@@ -1,24 +1,20 @@
-﻿using System.Collections;
+﻿#region License
+// ====================================================
+// Project Porcupine Copyright(C) 2016 Team Porcupine
+// This program comes with ABSOLUTELY NO WARRANTY; This is free software, 
+// and you are welcome to redistribute it under certain conditions; See 
+// file LICENSE, which is part of this source code package, for details.
+// ====================================================
+#endregion
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipSpriteController : BaseSpriteController<Ship> {
-
-	// Use this for initialization
-	void Start () {
-        ShipManager shipManager = World.Current.shipManager;
-        shipManager.ShipCreated += OnCreated;
-        shipManager.ShipRemoved += OnRemoved;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    
-	}
-
+public class ShipSpriteController : BaseSpriteController<Ship>
+{
     public ShipSpriteController(World world) : base(world, "Ships")
     {
-        
     }
 
     protected override void OnCreated(Ship ship)
@@ -29,7 +25,7 @@ public class ShipSpriteController : BaseSpriteController<Ship> {
         objectGameObjectMap.Add(ship, ship_go);
 
         ship_go.name = "Ship";
-        // ship_go.transform.position = new Vector3(c.X, c.Y, c.Z);
+        //// ship_go.transform.position = new Vector3(c.X, c.Y, c.Z);
         ship_go.transform.SetParent(objectParent.transform, true);
 
         SpriteRenderer sr = ship_go.AddComponent<SpriteRenderer>();
@@ -40,12 +36,10 @@ public class ShipSpriteController : BaseSpriteController<Ship> {
     }
 
     protected override void OnChanged(Ship ship)
-    {
-        
+    { 
     }
 
     protected override void OnRemoved(Ship ship)
-    {
-        
+    {   
     }
 }
