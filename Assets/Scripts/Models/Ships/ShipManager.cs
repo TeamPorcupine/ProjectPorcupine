d159351c89a717af27b289977c4205256134dfea
abackwood <a.backwood@gmail.com>
2016-09-04 10:33:03 +0200

    Merged conflict

---
+++ Assets/Scripts/Models/Ships/ShipManager.cs
@@ -0,0 +1,30 @@
+﻿#region License
+// ====================================================
+// Project Porcupine Copyright(C) 2016 Team Porcupine
+// This program comes with ABSOLUTELY NO WARRANTY; This is free software, 
+// and you are welcome to redistribute it under certain conditions; See 
+// file LICENSE, which is part of this source code package, for details.
+// ====================================================
+#endregion
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipManager
+{
+    private Dictionary<string, Ship> shipPrototypes;
+    private List<Ship> shipsInWorld;
+
+    public ShipManager()
+    {
+        shipPrototypes = new Dictionary<string, Ship>();
+        shipsInWorld = new List<Ship>();
+    }
+
+    public Ship AddShip(string type, Vector2 position)
+    {
+        Ship ship = new Ship(shipPrototypes[type]);
+        shipsInWorld.Add(ship);
+        return ship;
+    }
+}
---
+++ Assets/Scripts/Models/Ships/ShipStorage.cs
@@ -0,0 +1,21 @@
+﻿#region License
+// ====================================================
+// Project Porcupine Copyright(C) 2016 Team Porcupine
+// This program comes with ABSOLUTELY NO WARRANTY; This is free software, 
+// and you are welcome to redistribute it under certain conditions; See 
+// file LICENSE, which is part of this source code package, for details.
+// ====================================================
+#endregion
+
+public class ShipStorage
+{
+    public ShipStorage(int x, int y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+
+    public int X { get; private set; }
+
+    public int Y { get; private set; }
+}
