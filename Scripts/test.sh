e7f7084ac950033c585f454e9c6a356859eac83b
Brian Jubelirer <brian2386@gmail.com>
2016-09-05 00:06:22 -0400

    Show compile errors clearly in Travis CI (#1138)
    * spit compiler error
    
    * correctly parse error
    
    * compare output to empty string not 0
    
    * keep new lines in console output
    
    * swap sed for awk
    
    * don't store mutliline output in variable
    
    * add newline before build failed, fix spacing line 16

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+endTestsFold=0 #stores whether the travis_fold:end:tests has been echoed yet
+
 echo "Attempting Unit Tests"
 echo 'travis_fold:start:compile'
 /Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -runEditorTests -nographics -EditorTestResultFile $(pwd)/EditorTestResults.xml -projectPath $(pwd) -logFile unity.log 
@@ -10,38 +12,54 @@ echo 'Show Results from Tests'
 echo 'travis_fold:start:tests'
 if [ ! -f $(pwd)/EditorTestResults.xml ]; then
     echo "Results file not found!"
-	exit 1
+    echo "travis_fold:end:tests"
+    $endTestsFold=1
+
+    # at this point we know that the build has failed due to compilation errors
+    # lets try to parse them out of unity.log and display them
+    if [ -f $(pwd)/unity.log ]; then
+        out=$(grep "CompilerOutput" unity.log)
+        if [ "$out" != "" ]; then
+
+            echo '\nBuild Failed! \nThe compiler generated the following messages:'
+            echo | awk '/CompilerOutput:/,/EndCompilerOutput/' < unity.log #show lines in between compiler output "tags" including tags 
+
+        fi
+    fi
+    exit 1
 fi
 cat $(pwd)/EditorTestResults.xml
-echo 'travis_fold:end:tests'
+if [ "$endTestsFold" == 0 ]; then
+    echo 'travis_fold:end:tests'
+fi
 
 #TERRIBLE error check logic - Please fix ASAP
 errorCount=$(grep "failures" EditorTestResults.xml | awk -F"\"" '{print $8}') #find line with 'failures' and returns characters between quotation mark 8 and 9
 
 if [ "$errorCount" != "0" ]; then
-	echo $errorCount ' unit tests failed!'
-	exit 1
+    echo $errorCount ' unit tests failed!'
+    exit 1
 fi
 
 errorCount=$(grep "failures" EditorTestResults.xml | awk -F"\"" '{print $6}') #now for errors
 
 if [ "$errorCount" != "0" ]; then
-	echo $errorCount ' unit tests threw errors!'
-	exit 1
+    echo $errorCount ' unit tests threw errors!'
+    exit 1
 fi
 
 errorCount=$(grep "failures" EditorTestResults.xml | awk -F"\"" '{print $12}') #inconlusive tests
 
 if [ "$errorCount" != "0" ]; then
-	echo $errorCount ' unit tests were inconlusive!'
-	exit 1
+    echo $errorCount ' unit tests were inconlusive!'
+    exit 1
 fi
 
 
 errorCount=$(grep "failures" EditorTestResults.xml | awk -F"\"" '{print $18}') #finally for invalid tests
 
 if [ "$errorCount" != "0" ]; then
-	echo $errorCount ' unit tests were invalid!'
-	exit 1
+    echo $errorCount ' unit tests were invalid!'
+    exit 1
 fi
 #end of unit test checks. at this point the test have suceeded or exited with an error code.
