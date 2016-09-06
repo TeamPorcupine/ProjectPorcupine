06081c1a8e975b1e6760e97a7127a331c97e0cd3
Steve Zvosec <szvosec@gmail.com>
2016-09-06 19:20:06 -0400

    Adds Radioisotope Thermoelectric  Generator - RTG

--- Assets/StreamingAssets/LUA/Furniture.lua
+++ Assets/StreamingAssets/LUA/Furniture.lua
@@ -550,14 +550,14 @@ function LandingPad_Test_CallTradeShip(furniture, character)
    WorldController.Instance.TradeController.CallTradeShipTest(furniture)
 end
 
--- This function gets called once, when the funriture is isntalled
+-- This function gets called once, when the furniture is installed
 function Heater_InstallAction( furniture, deltaTime)
     -- TODO: find elegant way to register heat source and sinks to Temperature
 	furniture.EventActions.Register("OnUpdateTemperature", "Heater_UpdateTemperature")
 	World.Current.temperature.RegisterSinkOrSource(furniture)
 end
 
--- This function gets called once, when the funriture is unisntalled
+-- This function gets called once, when the furniture is uninstalled
 function Heater_UninstallAction( furniture, deltaTime)
     furniture.EventActions.Deregister("OnUpdateTemperature", "Heater_UpdateTemperature")
 	World.Current.temperature.DeregisterSinkOrSource(furniture)
@@ -657,6 +657,35 @@ end
 
 function OreMine_GetSpriteName(furniture)
     return "mine_" .. furniture.Parameters["ore_type"].ToString()
+end
+
+-- This function gets called once, when the furniture is installed
+function Rtg_InstallAction( furniture, deltaTime)
+    -- TODO: find elegant way to register heat source and sinks to Temperature
+	furniture.EventActions.Register("OnUpdateTemperature", "Rtg_UpdateTemperature")
+	World.Current.temperature.RegisterSinkOrSource(furniture)
+end
+
+-- This function gets called once, when the furniture is uninstalled
+function Rtg_UninstallAction( furniture, deltaTime)
+    furniture.EventActions.Deregister("OnUpdateTemperature", "Rtg_UpdateTemperature")
+	World.Current.temperature.DeregisterSinkOrSource(furniture)
+	-- TODO: find elegant way to unregister previous register
+end
+
+function Rtg_UpdateTemperature( furniture, deltaTime)
+    if (furniture.tile.Room.IsOutsideRoom() == true) then
+        return
+    end
+    
+    tile = furniture.tile
+    pressure = tile.Room.GetGasPressure() / tile.Room.GetSize()
+    efficiency = ModUtils.Clamp01(pressure / furniture.Parameters["pressure_threshold"].ToFloat())
+    temperatureChangePerSecond = furniture.Parameters["base_heating"].ToFloat() * efficiency
+    temperatureChange = temperatureChangePerSecond * deltaTime
+    
+    World.Current.temperature.ChangeTemperature(tile.X, tile.Y, temperatureChange)
+    --ModUtils.ULogChannel("Temperature", "Heat change: " .. temperatureChangePerSecond .. " => " .. World.current.temperature.GetTemperature(tile.X, tile.Y))
 end
 
 ModUtils.ULog("Furniture.lua loaded")
