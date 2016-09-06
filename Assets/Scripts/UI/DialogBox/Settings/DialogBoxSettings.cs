9b62c4f8c28b7de325fc5e6d901e94782f7abb42
James <Novajames276@gmail.com>
2016-09-06 19:59:03 +0100

    Rework of FurnitureBuildMenu (#1183)
    * Reworked the Build menu so that it can be rebuilt on the fly
    
    Also connected to dev mode so that it can show all the furniture or just the ones the player would normaly see.
    
    * Made showAllFurniture bool optional

--- Assets/Scripts/UI/DialogBox/Settings/DialogBoxSettings.cs
+++ Assets/Scripts/UI/DialogBox/Settings/DialogBoxSettings.cs
@@ -90,6 +90,7 @@ public class DialogBoxSettings : DialogBox
     {
         this.CloseDialog();
         WorldController.Instance.spawnInventoryController.SetUIVisibility(developerModeToggle.isOn);
+        FurnitureBuildMenu.instance.RebuildMenuButtons(developerModeToggle.isOn);
         SaveSetting();
     }
 
--- Assets/Scripts/UI/FurnitureBuildMenu.cs
+++ Assets/Scripts/UI/FurnitureBuildMenu.cs
@@ -6,58 +6,37 @@
 // file LICENSE, which is part of this source code package, for details.
 // ====================================================
 #endregion
-using System.Linq;
+using System.Collections.Generic;
 using ProjectPorcupine.Localization;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class FurnitureBuildMenu : MonoBehaviour
 {
+    public static FurnitureBuildMenu instance;
     public GameObject buildFurnitureButtonPrefab;
 
+    private List<GameObject> buildMenu;
     private string lastLanguage;
+    private bool showAllFurniture;
 
-    // Use this for initialization
-    private void Start()
+    public void RebuildMenuButtons(bool showAllFurniture = false)
     {
-        BuildModeController bmc = WorldController.Instance.buildModeController;
-
-        // For each furniture prototype in our world, create one instance
-        // of the button to be clicked!
-        foreach (string furnitureKey in PrototypeManager.Furniture.Keys)
+        foreach (GameObject gameObject in buildMenu)
         {
-            if (PrototypeManager.Furniture.Get(furnitureKey).HasTypeTag("Non-buildable"))
-            {
-                continue;
-            }
-
-            GameObject gameObject = (GameObject)Instantiate(buildFurnitureButtonPrefab);
-            gameObject.transform.SetParent(this.transform);
-
-            Furniture proto = PrototypeManager.Furniture.Get(furnitureKey);
-            string objectId = furnitureKey;
-
-            gameObject.name = "Button - Build " + objectId;
-
-            gameObject.transform.GetComponentInChildren<TextLocalizer>().formatValues = new string[] { LocalizationTable.GetLocalization(proto.LocalizationCode) };
-
-            Button button = gameObject.GetComponent<Button>();
-
-            button.onClick.AddListener(delegate
-                {
-                    bmc.SetMode_BuildFurniture(objectId);
-                    this.gameObject.SetActive(false);
-                });
-
-            // http://stackoverflow.com/questions/1757112/anonymous-c-sharp-delegate-within-a-loop
-            string furniture = furnitureKey;
-            LocalizationTable.CBLocalizationFilesChanged += delegate
-            {
-                gameObject.transform.GetComponentInChildren<TextLocalizer>().formatValues = new string[] { LocalizationTable.GetLocalization(PrototypeManager.Furniture.Get(furniture).LocalizationCode) };
-            };
+            Destroy(gameObject);
         }
 
-        lastLanguage = LocalizationTable.currentLanguage;
+        this.showAllFurniture = showAllFurniture;
+
+        GenerateMenuButtons();
+    }
+
+    private void Start()
+    {
+        instance = this;
+        showAllFurniture = Settings.GetSetting("DialogBoxSettings_developerModeToggle", false);
+        GenerateMenuButtons();        
     }
 
     private void Update()
@@ -73,5 +52,50 @@ public class FurnitureBuildMenu : MonoBehaviour
                 localizers[i].UpdateText(LocalizationTable.GetLocalization(PrototypeManager.Furniture.Get(i).GetName()));
             }
         }
+    }
+
+    private void GenerateMenuButtons()
+    {
+        BuildModeController bmc = WorldController.Instance.buildModeController;
+
+        buildMenu = new List<GameObject>();
+
+        // For each furniture prototype in our world, create one instance
+        // of the button to be clicked!
+        foreach (string furnitureKey in PrototypeManager.Furniture.Keys)
+        {
+            if (PrototypeManager.Furniture.Get(furnitureKey).HasTypeTag("Non-buildable") && showAllFurniture == false)
+            {
+                continue;
+            }
+
+            GameObject gameObject = (GameObject)Instantiate(buildFurnitureButtonPrefab);
+            gameObject.transform.SetParent(this.transform);
+            buildMenu.Add(gameObject);
+
+            Furniture proto = PrototypeManager.Furniture.Get(furnitureKey);
+            string objectId = furnitureKey;
+
+            gameObject.name = "Button - Build " + objectId;
+
+            gameObject.transform.GetComponentInChildren<TextLocalizer>().formatValues = new string[] { LocalizationTable.GetLocalization(proto.LocalizationCode) };
+
+            Button button = gameObject.GetComponent<Button>();
+
+            button.onClick.AddListener(delegate
+            {
+                bmc.SetMode_BuildFurniture(objectId);
+                this.gameObject.SetActive(false);
+            });
+
+            // http://stackoverflow.com/questions/1757112/anonymous-c-sharp-delegate-within-a-loop
+            string furniture = furnitureKey;
+            LocalizationTable.CBLocalizationFilesChanged += delegate
+            {
+                gameObject.transform.GetComponentInChildren<TextLocalizer>().formatValues = new string[] { LocalizationTable.GetLocalization(PrototypeManager.Furniture.Get(furniture).LocalizationCode) };
+            };
+        }
+
+        lastLanguage = LocalizationTable.currentLanguage;
     }
 }
