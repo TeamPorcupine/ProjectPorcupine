590af020ca1cea6c3d5cad5efd89a3efd2a816e9
Ben Pearson <tuvas21@gmail.com>
2016-09-06 09:22:31 -0400

    Adds blockRaycast, to make sure this won't influence anything else.

--- Assets/Scripts/UI/HeadlineController.cs
+++ Assets/Scripts/UI/HeadlineController.cs
@@ -26,6 +26,7 @@ public class HeadlineController : MonoBehaviour
     {
         canvasGroup.alpha = 0;
         canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
     }
 
     private void Awake()
@@ -49,6 +50,7 @@ public class HeadlineController : MonoBehaviour
     {
         canvasGroup.alpha = 1;
         canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
 
         Debug.ULogChannel("Headline", newHeadline);
         textBox.text = newHeadline;
