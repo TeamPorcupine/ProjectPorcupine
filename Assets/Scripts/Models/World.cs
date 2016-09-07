2913a2d99d95efe5624e4e983d27351ef299a73a
Koosemose <koosemose@gmail.com>
2016-09-07 09:04:18 -0400

    Fix loading character inventory from save

--- Assets/Scripts/Models/World.cs
+++ Assets/Scripts/Models/World.cs
@@ -834,29 +834,51 @@ public class World : IXmlSerializable
 
                 character.name = reader.GetAttribute("name");
                 character.ReadXml(reader);
-                if (reader.ReadToDescendant("Inventories")) 
+
+                // Read the children elements.
+                // TODO: This should either not be XML, or use XmlSerializer.
+                while (reader.Read())
                 {
-                    if (reader.ReadToDescendant("Inventory"))
+                    // Read until the end of the character.
+                    if (reader.NodeType == XmlNodeType.EndElement)
                     {
-                        do
-                        {
-                            // Create our inventory from the file
-                            Inventory inv = new Inventory(
-                                reader.GetAttribute("objectType"),
-                                int.Parse(reader.GetAttribute("maxStackSize")),
-                                int.Parse(reader.GetAttribute("stackSize")));
+                        break;
+                    }
 
-                            inventoryManager.PlaceInventory(character, inv);
-                        }
-                        while (reader.ReadToNextSibling("Inventory"));
+                    switch (reader.Name)
+                    {
+                        case "Stats":
+                            character.ReadStatsFromSave(reader);
+                            break;
+                        case "Inventories":
+                            if (reader.ReadToDescendant("Inventory"))
+                            {
+                                do
+                                {
+                                    // Create our inventory from the file
+                                    Inventory inv = new Inventory(
+                                        reader.GetAttribute("objectType"),
+                                        int.Parse(reader.GetAttribute("maxStackSize")),
+                                        int.Parse(reader.GetAttribute("stackSize")));
 
-                        // One more read to step out of Inventories, so ReadToNextSibling will find sibling Character
-                        reader.Read();
+                                    inventoryManager.PlaceInventory(character, inv);
+                                }
+                                while (reader.ReadToNextSibling("Inventory"));
+
+                                // One more read to step out of Inventories, so ReadToNextSibling will find sibling Character
+                                reader.Read();
+                            }
+                            break;
                     }
                 }
             }
             while (reader.ReadToNextSibling("Character"));
         }
+    }
+
+    private void ReadCharacterInventoryFromSave(XmlReader reader)
+    {
+
     }
 
 #region TestFunctions
