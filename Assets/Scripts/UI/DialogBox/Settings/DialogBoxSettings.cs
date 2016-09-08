0a71bdf24b8b19da0150cd5fa62ff0cbebf9a106
jaselito <jslopez68@hotmail.com>
2016-09-08 14:59:31 -0500

    Disable antialias - fix #1213 (#1231)
    * Disabled antialias in Project Settings -> Quality
    
    * Removed antialias entries from scripts and settings files
    
    * Removed antialias entry from settings dialog

--- Assets/Scripts/UI/DialogBox/Settings/DialogBoxSettings.cs
+++ Assets/Scripts/UI/DialogBox/Settings/DialogBoxSettings.cs
@@ -29,7 +29,6 @@ public class DialogBoxSettings : DialogBox
     public Resolution[] resolutions;
     public Dropdown resolutionDropdown;
 
-    public Dropdown aliasingDropdown;
     public Dropdown vsyncDropdown;
     public Dropdown qualityDropdown;
 
@@ -71,11 +70,6 @@ public class DialogBoxSettings : DialogBox
         /// TODO : Implement Resolution changes.
     }
 
-    public void OnAliasingChange()
-    {
-        /// TODO : Implement AA changes.
-    }
-
     public void OnMusicChange()
     {
         /// TODO : Implement Music changes.
@@ -109,7 +103,6 @@ public class DialogBoxSettings : DialogBox
         Settings.SetSetting("DialogBoxSettings_qualityDropdown", qualityDropdown.value);
         Settings.SetSetting("DialogBoxSettings_vSyncDropdown", vsyncDropdown.value);
         Settings.SetSetting("DialogBoxSettings_resolutionDropdown", resolutionDropdown.value);
-        Settings.SetSetting("DialogBoxSettings_aliasingDropdown", aliasingDropdown.value);
     }
 
     private void OnEnable()
@@ -142,10 +135,6 @@ public class DialogBoxSettings : DialogBox
         resolutionDropdown.onValueChanged.AddListener(delegate
         {
             OnResolutionChange();
-        });
-        aliasingDropdown.onValueChanged.AddListener(delegate
-        {
-            OnAliasingChange();
         });
         vsyncDropdown.onValueChanged.AddListener(delegate
         {
@@ -183,7 +172,6 @@ public class DialogBoxSettings : DialogBox
         qualityDropdown.value = Settings.GetSetting("DialogBoxSettings_qualityDropdown", 0);
         vsyncDropdown.value = Settings.GetSetting("DialogBoxSettings_vSyncDropdown", 0);
         resolutionDropdown.value = Settings.GetSetting("DialogBoxSettings_resolutionDropdown", 0);
-        aliasingDropdown.value = Settings.GetSetting("DialogBoxSettings_aliasingDropdown", 0);
     }
 
     private void CreateResDropDown()
