2a47239fe8da3a5efb3ddad8348a0cb7d9d00b77
Tomás Malbrán <TomMalbran@users.noreply.github.com>
2016-09-11 20:20:24 -0300

    New CharacterNameManager (#1259)
    * New CharacterNameManager
    
    * Fixed comments and randomizer
    
    * Revert randomizer
    
    * Simplified randomizer

--- Assets/Scripts/Controllers/ModsManager.cs
+++ Assets/Scripts/Controllers/ModsManager.cs
@@ -25,12 +25,12 @@ public class ModsManager
 
     public void LoadPrototypes()
     {
-        LoadFunctionScripts("Furniture", "Furniture.lua");
-        LoadFunctionScripts("Need", "Need.lua");
-        LoadFunctionScripts("GameEvent", "GameEvent.lua");
-        LoadFunctionScripts("TileType", "Tiles.lua");
-        LoadFunctionScripts("Quest", "Quest.lua");
-        LoadFunctionScripts("ScheduledEvent", "ScheduledEvent.lua");
+        LoadFunctions("Furniture", "Furniture.lua");
+        LoadFunctions("Need", "Need.lua");
+        LoadFunctions("GameEvent", "GameEvent.lua");
+        LoadFunctions("TileType", "Tiles.lua");
+        LoadFunctions("Quest", "Quest.lua");
+        LoadFunctions("ScheduledEvent", "ScheduledEvent.lua");
 
         PrototypeManager.Furniture.LoadPrototypes(mods);
         PrototypeManager.Inventory.LoadPrototypes(mods);
@@ -39,6 +39,8 @@ public class ModsManager
         PrototypeManager.SchedulerEvent.LoadPrototypes(mods);
         PrototypeManager.Stat.LoadPrototypes(mods);
         PrototypeManager.Quest.LoadPrototypes(mods);
+
+        LoadCharacterNames("CharacterNames.txt");
     }
 
     public DirectoryInfo[] GetMods()
@@ -47,18 +49,52 @@ public class ModsManager
     }
 
     /// <summary>
-    /// Loads the base and the mods scripts.
+    /// Loads all the functions from the given script.
     /// </summary>
-    /// <param name="mods">The mods directories.</param>
+    /// <param name="functionsName">The functions name.</param>
     /// <param name="fileName">The file name.</param>
-    private void LoadFunctionScripts(string functionsName, string fileName)
+    private void LoadFunctions(string functionsName, string fileName)
     {
-        string filePath = Path.Combine(Application.streamingAssetsPath, "LUA");
+        LoadTextFile(
+            "LUA",
+            fileName,
+            (filePath) =>
+            {
+                string text = File.ReadAllText(filePath);
+                FunctionsManager.Get(functionsName).LoadScript(text, functionsName);
+            });
+    }
+
+    /// <summary>
+    /// Loads all the character names from the given file.
+    /// </summary>
+    /// <param name="fileName">The file name.</param>
+    private void LoadCharacterNames(string fileName)
+    {
+        LoadTextFile(
+            "Data",
+            fileName,
+            (filePath) =>
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                CharacterNameManager.LoadNames(lines);
+            });
+    }
+
+    /// <summary>
+    /// Loads the given file from the given folder in the base and inside the mods and
+    /// calls the Action with the file path.
+    /// </summary>
+    /// <param name="folderName">Folder name.</param>
+    /// <param name="fileName">File name.</param>
+    /// <param name="readText">Called to handle the text reading and actual loading.</param>
+    private void LoadTextFile(string folderName, string fileName, Action<string> readText)
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, folderName);
         filePath = Path.Combine(filePath, fileName);
         if (File.Exists(filePath))
         {
-            string text = File.ReadAllText(filePath);
-            FunctionsManager.Get(functionsName).LoadScript(text, functionsName);
+            readText(filePath);
         }
 
         foreach (DirectoryInfo mod in mods)
@@ -66,8 +102,7 @@ public class ModsManager
             filePath = Path.Combine(mod.FullName, fileName);
             if (File.Exists(filePath))
             {
-                string text = File.ReadAllText(filePath);
-                FunctionsManager.Get(functionsName).LoadScript(text, functionsName);
+                readText(filePath);
             }
         }
     }
