7342a7498bc0f125b0c1d8ddfe0d916785e383da
Koosemose <koosemose@gmail.com>
2016-09-12 02:44:21 -0400

    Remove Debugs

--- Assets/Scripts/Controllers/FurnitureSpriteController.cs
+++ Assets/Scripts/Controllers/FurnitureSpriteController.cs
@@ -26,7 +26,6 @@ public class FurnitureSpriteController : BaseSpriteController<Furniture>
         // Go through any EXISTING furniture (i.e. from a save that was loaded OnEnable) and call the OnCreated event manually.
         foreach (Furniture furn in world.furnitures)
         {
-            Debug.Log(furn.Name);
             OnCreated(furn);
         }
     }
