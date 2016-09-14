356663c0ea3207a8d6a469a4d0e8d1b8395f53a0
Ben Pearson <tuvas21@gmail.com>
2016-09-14 13:22:57 -0400

    Improved the default "single" implementation

--- Assets/Scripts/Controllers/BuildModeController.cs
+++ Assets/Scripts/Controllers/BuildModeController.cs
@@ -41,7 +41,7 @@ public class BuildModeController
 
         Furniture proto = PrototypeManager.Furniture.Get(buildModeType);
 
-        return proto.Width == 1 && proto.Height == 1;
+        return proto.DragType!="single";
     }
 
     public string GetFloorTile()
