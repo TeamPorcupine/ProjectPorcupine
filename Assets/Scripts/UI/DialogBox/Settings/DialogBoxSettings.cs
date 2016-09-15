70c29eccb883b0525a88aa1e123694b4900aae44
kd7uiy <tuvas21@gmail.com>
2016-09-15 19:48:45 -0400

    Fix DialogBox Stylecop Violation (#1337)

--- Assets/Scripts/UI/DialogBox/Settings/DialogBoxSettings.cs
+++ Assets/Scripts/UI/DialogBox/Settings/DialogBoxSettings.cs
@@ -7,9 +7,9 @@
 // ====================================================
 #endregion
 using System.Collections.Generic;
+using ProjectPorcupine.Localization;
 using UnityEngine;
 using UnityEngine.UI;
-using ProjectPorcupine.Localization;
 
 public class DialogBoxSettings : DialogBox
 {
