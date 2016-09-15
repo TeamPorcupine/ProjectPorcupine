9a2d9d1a0a4649cd89348cbf53321472165028e1
kd7uiy <tuvas21@gmail.com>
2016-09-15 10:31:22 -0400

    Correctly name "Requiere" to "Require" (#1332)
    * Correctly name "Require"
    
    * Fixed order of options

--- Assets/Scripts/Models/Furniture.cs
+++ Assets/Scripts/Models/Furniture.cs
@@ -617,7 +617,7 @@ public class Furniture : IXmlSerializable, ISelectable, IPrototypable, IContextA
                     {
                         LuaFunction = reader.GetAttribute("FunctionName"),
                         Text = reader.GetAttribute("Text"),
-                        RequiereCharacterSelected = bool.Parse(reader.GetAttribute("RequiereCharacterSelected")),
+                        RequireCharacterSelected = bool.Parse(reader.GetAttribute("RequireCharacterSelected")),
                         DevModeOnly = bool.Parse(reader.GetAttribute("DevModeOnly") ?? "false")
                     });
                     break;
@@ -964,7 +964,7 @@ public class Furniture : IXmlSerializable, ISelectable, IPrototypable, IContextA
                 yield return new ContextMenuAction
                 {
                     Text = contextMenuLuaAction.Text,
-                    RequireCharacterSelected = contextMenuLuaAction.RequiereCharacterSelected,
+                    RequireCharacterSelected = contextMenuLuaAction.RequireCharacterSelected,
                     Action = (cma, c) => InvokeContextMenuLuaAction(contextMenuLuaAction.LuaFunction, c)
                 };
             }
