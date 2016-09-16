d2cb5afc156c6b93b347d970e5c90e5bea05269a
longtomjr <longtomjr@gmail.com>
2016-09-16 19:38:47 +0200

    Giving up now, last commit for a bit

---
+++ Assets/Scripts/Controllers/AudioManager.cs
@@ -0,0 +1,102 @@
+﻿#region License
+// ====================================================
+// Project Porcupine Copyright(C) 2016 Team Porcupine
+// This program comes with ABSOLUTELY NO WARRANTY; This is free software, 
+// and you are welcome to redistribute it under certain conditions; See 
+// file LICENSE, which is part of this source code package, for details.
+// ====================================================
+#endregion
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class AudioManager
+{
+    private static Dictionary<string, AudioClip> audioClips;
+
+    public AudioManager()
+    {
+        audioClips = new Dictionary<string, AudioClip>();
+    }
+
+    public static void LoadAudioFiles(string directoryPath)
+    {
+        string[] subDirectories = Directory.GetDirectories(directoryPath);
+        foreach (string subDirectory in subDirectories)
+        {
+            LoadAudioFiles(subDirectory);
+        }
+
+        string[] filesInDir = Directory.GetFiles(directoryPath);
+        foreach (string filePath in filesInDir)
+        {
+            string audioCategory = new DirectoryInfo(directoryPath).Name;
+            string filename = new FileInfo(filePath).Name;
+            IEnumerator e = LoadAudioFile(audioCategory, filePath);
+            e.MoveNext();
+        }
+    }
+
+    private static IEnumerator LoadAudioFile(string audioCategory, string filePath)
+    {
+        if (filePath.Contains(".xml") || filePath.Contains(".meta") || filePath.Contains(".db"))
+        {
+            yield break;
+        }
+        WWW www = new WWW(@"file://" + filePath);
+
+        Debug.Log("before - " + www.error);
+
+        yield return www;
+
+        Debug.Log("After - " + www.error);
+
+        AudioClip clip = www.GetAudioClip(false, true);
+
+        Debug.Log(clip.name + "Downloaded");
+
+        string filename = new FileInfo(filePath).Name;
+
+        audioClips[filename] = clip;
+        // filename = audioCategory + "/" + filename;
+
+
+    }
+
+    public static string GetDebugString(Dictionary<string, AudioClip> dictionary)
+    {
+        return "{" + string.Join(",", dictionary.Select(kv => kv.Key + "=" + kv.Value.length).ToArray()) + "}";
+    }
+
+    public static AudioClip GetAudio(string categoryName, string audioName)
+    {
+        AudioClip clip = new AudioClip();
+
+        //string audioNameAndCategory = categoryName + "/" + audioName;
+
+        if (audioClips.ContainsKey(audioName))
+        {
+            clip = audioClips[audioName];
+        }
+        else
+        {
+            try
+            {
+                clip = audioClips["Sound/Error.wav"];
+            }
+            catch
+            {
+                string str = GetDebugString(audioClips);
+                throw new UnityException(str);
+            }
+
+
+           }
+
+
+        return clip;
+    }
+
+}
