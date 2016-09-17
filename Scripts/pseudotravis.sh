606bbeb2bdc7089a7f29d904700d0971f3aef0d8
mikejbrown <michael.brown6@my.jcu.edu.au>
2016-09-17 13:46:42 +1000

    Add a local "Travis" script useful for a pre-commit or pre-push hook.
    Scripts/pseudotravis.sh is closely based on the actual Travis build script
    Scripts/test.sh, but runs locally. Before using as a git hook script it
    must be configured with your local Unity executable path.
    
    To do so change the localUnity env variable at the top of the script.
    To use as a pre-commit or pre-push hook simply copy or symlink to the
    relevant place in .git/hooks/.

---
+++ Scripts/pseudotravis.sh
@@ -0,0 +1,71 @@
+#! /bin/sh
+
+localUnity="C:\\Program Files\\Unity\\Editor\\Unity.exe"
+endTestsFold=0 #stores whether the travis_fold:end:tests has been echoed yet
+
+echo "Attempting Unit Tests"
+"$localUnity" -batchmode -runEditorTests -nographics -EditorTestResultFile $(pwd)/EditorTestResults.xml -projectPath $(pwd) -logFile unity.log 
+#cat $(pwd)/unity.log
+
+if [ ! -f $(pwd)/EditorTestResults.xml ]; then
+    echo "Results file not found!"
+    $endTestsFold=1
+
+    # at this point we know that the build has failed due to compilation errors
+    # lets try to parse them out of unity.log and display them
+    if [ -f $(pwd)/unity.log ]; then
+        out=$(grep "CompilerOutput" unity.log)
+        if [ "$out" != "" ]; then
+
+            echo '\nBuild Failed! \nThe compiler generated the following messages:'
+            echo | awk '/CompilerOutput:/,/EndCompilerOutput/' < unity.log #show lines in between compiler output "tags" including tags 
+
+        fi
+	rm $(pwd)/unity.log
+    fi
+    exit 1
+fi
+#cat $(pwd)/EditorTestResults.xml
+
+rm $(pwd)/unity.log
+#TERRIBLE error check logic - Please fix ASAP
+errorCount=$(grep "failures" EditorTestResults.xml | awk -F"\"" '{print $8}') #find line with 'failures' and returns characters between quotation mark 8 and 9
+
+if [ "$errorCount" != "0" ]; then
+    echo $errorCount ' unit tests failed!'
+     
+     #show the exact unit test failure
+    echo '\nThe following unit tests failed:'
+    echo | grep 'success="False"' EditorTestResults.xml | grep 'test-case'
+   
+    rm $(pwd)/EditorTestResults.xml
+    exit 1
+fi
+
+errorCount=$(grep "failures" EditorTestResults.xml | awk -F"\"" '{print $6}') #now for errors
+
+if [ "$errorCount" != "0" ]; then
+    echo $errorCount ' unit tests threw errors!'
+    rm $(pwd)/EditorTestResults.xml
+    exit 1
+fi
+
+errorCount=$(grep "failures" EditorTestResults.xml | awk -F"\"" '{print $12}') #inconlusive tests
+
+if [ "$errorCount" != "0" ]; then
+    echo $errorCount ' unit tests were inconlusive!'
+    rm $(pwd)/EditorTestResults.xml
+    exit 1
+fi
+
+
+errorCount=$(grep "failures" EditorTestResults.xml | awk -F"\"" '{print $18}') #finally for invalid tests
+
+if [ "$errorCount" != "0" ]; then
+    echo $errorCount ' unit tests were invalid!'
+    rm $(pwd)/EditorTestResults.xml
+    exit 1
+fi
+
+rm $(pwd)/EditorTestResults.xml
+#end of unit test checks. at this point the test have suceeded or exited with an error code.
