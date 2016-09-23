f00bb46d42b1a2076c3a82c5a854a258e257fbb7
jaselito <jslopez68@hotmail.com>
2016-09-23 16:53:22 -0500

    Load and save dialogs only list files with the .sav extension

--- Assets/Scripts/UI/DialogBox/FileSaveLoad/DialogBoxLoadSaveGame.cs
+++ Assets/Scripts/UI/DialogBox/FileSaveLoad/DialogBoxLoadSaveGame.cs
@@ -49,7 +49,7 @@ public class DialogBoxLoadSaveGame : DialogBox
 
         DirectoryInfo saveDir = new DirectoryInfo(saveDirectoryPath);
 
-        FileInfo[] saveGames = saveDir.GetFiles().OrderByDescending(f => f.CreationTime).ToArray();
+        FileInfo[] saveGames = saveDir.GetFiles("*.sav").OrderByDescending(f => f.CreationTime).ToArray();
 
         // Our save dialog has an input field, which the fileListItems fill out for
         // us when we click on them
