1aee9053d569b9fc871f0110b0a1aad24c3ec6d6
Geoffrotism <hullgs@dukes.jmu.edu>
2016-10-06 10:58:04 -0400

    Added health to furniture

---
+++ Assets/Scripts/Models/Health/Health.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health {
+    public enum ResistanceType
+    {
+        fire,
+        laser,
+        explosive,
+        melee
+    }
+
+    public void DamageEntity(Object temp, ResistanceType resistance, int damageAmount)
+    {
+        Furniture furn = new Furniture();
+        if (temp.GetType().Equals(furn.GetType()))
+        {
+            Debug.ULogChannel("Health.cs", "This is a Furniture object.");
+        }
+    }
+
+    public void DamageCharacter(GameObject go, ResistanceType resistance, int damageAmount)
+    {
+        
+    }
+
+}
