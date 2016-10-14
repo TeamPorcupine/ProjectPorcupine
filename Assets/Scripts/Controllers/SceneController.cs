641a98079f152d6e7b79c7bdec007a122ef50ecf
Ohda <loic@moana-design.net>
2016-10-14 07:25:46 +0200

    Remove unused tag, export IntroScene when build game, add missing use

--- Assets/Scripts/Controllers/SceneController.cs
+++ Assets/Scripts/Controllers/SceneController.cs
@@ -6,6 +6,7 @@
 // file LICENSE, which is part of this source code package, for details.
 // ====================================================
 #endregion
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneController
