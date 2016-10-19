4ae5439367cc5b4789001300661ea95e5be0804a
Ohda <loic@moana-design.net>
2016-10-19 20:05:42 +0200

    Back to Intro Scene

--- Assets/Scripts/UI/DialogBox/Options/DialogBoxOptions.cs
+++ Assets/Scripts/UI/DialogBox/Options/DialogBoxOptions.cs
@@ -38,31 +38,10 @@ public class DialogBoxOptions : DialogBox
         dialogManager.dialogBoxSettings.ShowDialog();
     }
 
-    // Quit the app whether in editor or a build version.
     public void OnButtonQuitGame()
     {
-        StartCoroutine(ConfirmQuitDialog());
-    }
-
-    private IEnumerator ConfirmQuitDialog()
-    {
-        dialogManager.dialogBoxPromptOrInfo.SetPrompt("prompt_confirm_quit");
-        dialogManager.dialogBoxPromptOrInfo.SetButtons(DialogBoxResult.Yes, DialogBoxResult.No);
-
-        dialogManager.dialogBoxPromptOrInfo.Closed = () =>
-        {
-            if (dialogManager.dialogBoxPromptOrInfo.Result == DialogBoxResult.Yes)
-            {
-                SceneController.Instance.QuitGame();
-            }
-        };
-
-        dialogManager.dialogBoxPromptOrInfo.ShowDialog();
-
-        while (dialogManager.dialogBoxPromptOrInfo.gameObject.activeSelf)
-        {
-            yield return null;
-        }
+        this.CloseDialog();
+        dialogManager.dialogBoxQuitMenu.ShowDialog();
     }
 
     private IEnumerator CheckIfSaveGameBefore(string prompt)
--- Assets/Scripts/UI/DialogBox/QuitMenu/DialogBoxQuitMenu.cs
+++ Assets/Scripts/UI/DialogBox/QuitMenu/DialogBoxQuitMenu.cs
@@ -13,5 +13,40 @@ using UnityEngine.UI;
 
 public class DialogBoxQuitMenu : DialogBox
 {
+    private void Start()
+    {
+        UnityEngine.Object buttonPrefab = Resources.Load("UI/Components/MenuButton");
 
+        GameObject quitButton = CreateButtonGO(buttonPrefab, "QuitGame", "menu_quit_game");
+        quitButton.GetComponent<Button>().onClick.AddListener(delegate
+        {
+            SceneController.Instance.QuitGame();
+        });
+
+        GameObject mainMenuButton = CreateButtonGO(buttonPrefab, "Quit To Main Menu", "menu_quit_to_menu");
+        mainMenuButton.GetComponent<Button>().onClick.AddListener(delegate
+        {
+            this.CloseDialog();
+            SceneController.Instance.LoadMainMenu();
+        });
+
+        GameObject cancelButton = CreateButtonGO(buttonPrefab, "Cancel", "cancel");
+        cancelButton.GetComponent<Button>().onClick.AddListener(delegate
+        {
+            this.CloseDialog();
+        });
+    }
+
+    private GameObject CreateButtonGO(UnityEngine.Object buttonPrefab, string name, string localizationCode)
+    {
+        GameObject buttonGameObject = (GameObject)Instantiate(buttonPrefab);
+        buttonGameObject.transform.SetParent(this.transform.Find("Buttons").transform, false);
+        buttonGameObject.name = "Button " + name;
+
+        TextLocalizer textLocalizer = buttonGameObject.transform.GetComponentInChildren<TextLocalizer>();
+        textLocalizer.formatValues = new string[] { LocalizationTable.GetLocalization(localizationCode) };
+        textLocalizer.defaultText = localizationCode;
+
+        return buttonGameObject;
+    }
 }
