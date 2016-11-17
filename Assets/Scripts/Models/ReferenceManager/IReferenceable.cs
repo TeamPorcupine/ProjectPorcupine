b94d382111ad168bab4d3f9c50a0cbc6b1422749
David McDermott <David McDermott>
2016-11-17 03:02:10 -0500

    no changes only files reference manager added

---
+++ Assets/Scripts/Models/ReferenceManager/IReferenceable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public interface IReferenceable
+{
+    //this should be IReferenceManager but it is 
+    // better to hide the implementation of the 
+    // folowing methods from the consumer:
+    // HasLinkInstance
+    // AddLinkInstance
+    // RemoveLinkInstance
+    // FirstLinkInstance
+    ReferenceManager ReferenceManager { get; }
+}
+
