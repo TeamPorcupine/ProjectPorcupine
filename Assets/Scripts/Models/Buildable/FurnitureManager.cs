4814a86127d18a51ede649196166ae75bd2b52f2
Koosemose <koosemose@users.noreply.github.com>
2016-11-16 19:31:42 -0500

    Sort Usings in FurnitureManager

--- Assets/Scripts/Models/Buildable/FurnitureManager.cs
+++ Assets/Scripts/Models/Buildable/FurnitureManager.cs
@@ -10,9 +10,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using MoonSharp.Interpreter;
 using Newtonsoft.Json.Linq;
 using UnityEngine;
-using MoonSharp.Interpreter;
 
 [MoonSharpUserData]
 public class FurnitureManager : IEnumerable<Furniture>
