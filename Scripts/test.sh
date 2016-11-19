38e5ab2b0949d147107cc29ba3b05a7c8ceb2397
Koosemose <koosemose@gmail.com>
2016-11-18 22:20:41 -0500

    Report clean Stylecop

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -67,6 +67,10 @@ travecho 'travis_fold:start:stylecop'
 echo "Stylecopping"
 StylecopOutput=$(mono /opt/stylecop/StyleCopCmd.Console.exe -rd Assets/ -vt)
 StyleCopErrorCode=$?
+
+if [ "$StyleCopErrorCode" != "0" ]; then
+    echo '\nNo Stylecop violations were found.\n'
+fi
 travecho 'travis_fold:end:stylecop'
 
 travecho 'travis_fold:start:compile'
