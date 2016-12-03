622202c41a7c92c463d941ffaf585c356c02976f
bjubes <brian2386@gmail.com>
2016-12-02 22:34:33 -0500

    add folding to mono download

--- Scripts/Install/mono.sh
+++ Scripts/Install/mono.sh
@@ -6,12 +6,12 @@ echo 'Retrieving mono version'
 curl -o "mono-version.txt" "https://raw.githubusercontent.com/enckse/travis-mono-scripts/master/mono-version.txt"
 VER=$(cat "mono-version.txt")
 echo 'travis_fold:end:get-version'
-echo 
 
 # Download from mono-project.com
+echo 'travis_fold:start:download-mono'
 echo 'Downloading Mono'
 curl -o mono.pkg "http://download.mono-project.com/archive/$VER/macos-10-x86/MonoFramework-MDK-$VER.macos10.xamarin.x86.pkg"
-echo
+echo 'travis_fold:end:download-mono'
 
 # Install mono
 echo 'travis_fold:start:install-mono'
