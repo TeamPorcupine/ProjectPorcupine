67515417ba88ab94d1c16a552a77b136ba7454bc
BraedonWooding <braedonww@gmail.com>
2016-12-17 11:01:37 +1100

    Removed list for hashset

--- Assets/Scripts/Models/Character/Character.cs
+++ Assets/Scripts/Models/Character/Character.cs
@@ -81,7 +81,7 @@ public class Character : ISelectable, IContextActionProvider
     private State state;
 
     // List of global states that always run
-    private List<State> globalStates;
+    private HashSet<State> globalStates;
 
     // Queue of states that aren't important enough to interrupt, but should run soon
     private Queue<State> stateQueue;
@@ -101,7 +101,7 @@ public class Character : ISelectable, IContextActionProvider
         characterSkinColor = skinColor;
         InitializeCharacterValues();
         stateQueue = new Queue<State>();
-        globalStates = new List<State>
+        globalStates = new HashSet<State>
         {
             new NeedState(this)
         };
@@ -338,7 +338,7 @@ public class Character : ISelectable, IContextActionProvider
                 else
                 {
                     // TODO: Lack of job states should be more interesting. Maybe go to the pub and have a pint?
-                    SetState(new IdleState(this));            
+                    SetState(new IdleState(this));
                 }
             }
         }
