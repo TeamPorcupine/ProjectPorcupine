0998e32b601a4c9f24f3f7ce326083a453ff1407
Danny Wheeler <Danny Wheeler>
2016-12-22 23:22:53 +0000

    started ModMenu.DisplaySettings

--- Assets/Scripts/UI/InGameUI/SettingsMenu/ModMenu.cs
+++ Assets/Scripts/UI/InGameUI/SettingsMenu/ModMenu.cs
@@ -7,17 +7,30 @@ using Newtonsoft.Json.Linq;
 
 public static class ModMenu {
     public static Dictionary<string, bool> activeMods { get; private set; }
-	
+    static List<string> modDirs;
+
 	public static void Load ()
     {
+        modDirs = new List<string>();
         JObject modSettings = (JObject)JToken.ReadFrom(new JsonTextReader(File.OpenText(Path.Combine(Application.persistentDataPath, "ModSettings.json"))));
         JArray active = (JArray)modSettings["activeMods"];
         foreach (DirectoryInfo mod in ModsManager.GetModsFiles())
         {
             string modPath = Path.Combine(mod.FullName, "mod.json");
+            modDirs.Add(modPath);
             JObject modData = (JObject)JToken.ReadFrom(new JsonTextReader(File.OpenText(modPath)));
             string name = (string)modData["name"];
             activeMods.Add(name, active.Contains(name));
         }
 	}
+    public static void DisplaySettings(Transform parent)
+    {
+        Load();
+        foreach (string mod in modDirs)
+        {
+            JObject modData = (JObject)JToken.ReadFrom(new JsonTextReader(File.OpenText(mod)));
+            string name = (string)modData["name"];
+            string desc = (string)modData["desc"];
+        }
+    }
 }
