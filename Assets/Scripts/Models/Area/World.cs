ec3ed2d35e1d1461485e60a13806018e09ca2122
Dan54 <dannyjwheeler@hotmail.co.uk>
2016-12-29 11:09:16 +0000

    Saved mods with world

--- Assets/Scripts/Models/Area/World.cs
+++ Assets/Scripts/Models/Area/World.cs
@@ -282,6 +282,7 @@ public class World
     public JObject ToJson()
     {
         JObject worldJson = new JObject();
+        worldJson.Add("ActiveMods", ModMenu.WriteJSON(true));
         worldJson.Add("Width", Width.ToString());
         worldJson.Add("Height", Height.ToString());
         worldJson.Add("Depth", Depth.ToString());
