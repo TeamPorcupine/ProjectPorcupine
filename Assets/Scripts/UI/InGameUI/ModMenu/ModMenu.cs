85b26e7f4a4627e8f18a76e790b62e137cc9bc3a
Dan54 <dannyjwheeler@hotmail.co.uk>
2016-12-30 09:47:09 +0000

    Fixed doNotSave bool

--- Assets/Scripts/UI/InGameUI/ModMenu/ModMenu.cs
+++ Assets/Scripts/UI/InGameUI/ModMenu/ModMenu.cs
@@ -100,7 +100,7 @@ public static class ModMenu
                 bool noSave = (bool)(modData["doNotSave"] ?? false);
                 if (noSave)
                 {
-                    nonSaving.Add(mod.FullName);
+                    nonSaving.Add(name);
                 }
             }
 
