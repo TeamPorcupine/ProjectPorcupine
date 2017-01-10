6eeb93648cb71704b69d80b174d916ea673ff5f2
Günther <gunther.wullaert@hotmail.com>
2017-01-10 20:04:31 +0100

    Style copped

--- Assets/Scripts/UI/DialogBox/DialogBoxNewGame.cs
+++ Assets/Scripts/UI/DialogBox/DialogBoxNewGame.cs
@@ -17,7 +17,7 @@ public class DialogBoxNewGame : DialogBox
     public InputField Height;
     public InputField Width;
     public InputField Depth;
-	public InputField Seed;
+    public InputField Seed;
     public Toggle GenerateAsteroids;
     public InputField GeneratorInputField;
     public GameObject generatorListItemPrefab;
@@ -30,8 +30,8 @@ public class DialogBoxNewGame : DialogBox
         Width.onEndEdit.AddListener(delegate { VerifyNumericInput(Width); });
         Depth.onEndEdit.AddListener(delegate { VerifyNumericInput(Depth); });
 
-		// Generate Random Seed TODO: make it alphanumerical
-		Seed.text = UnityEngine.Random.Range(0, int.MaxValue).ToString();
+        // Generate Random Seed TODO: make it alphanumerical
+        Seed.text = UnityEngine.Random.Range(0, int.MaxValue).ToString();
 
         // Get list of files in save location
         string generatorDirectoryPath = GameController.Instance.GeneratorBasePath();
@@ -74,18 +74,20 @@ public class DialogBoxNewGame : DialogBox
         int height = int.Parse(Height.text);
         int width = int.Parse(Width.text);
         int depth = int.Parse(Depth.text);
-		// Try and parse seed as Integer if this is not possible then hash string as text
-		int seed = 0;
-		if(int.TryParse(Seed.text, out seed) == false)
-		{
-			seed = Seed.text.GetHashCode ();
-			Debug.LogWarning ("Converted " + Seed.text + " to hash " + seed);
-		}
-		string generatorFile = GeneratorInputField.text + ".xml";
+
+        // Try and parse seed as Integer if this is not possible then hash string as integer
+        int seed = 0;
+        if (int.TryParse(Seed.text, out seed) == false)
+        {
+            seed = Seed.text.GetHashCode();
+            Debug.LogWarning("Converted " + Seed.text + " to hash " + seed);
+        }
+
+        string generatorFile = GeneratorInputField.text + ".xml";
         DialogBoxManager dialogManager = GameObject.FindObjectOfType<DialogBoxManager>();
         dialogManager.dialogBoxPromptOrInfo.SetPrompt("message_creating_new_world");
         dialogManager.dialogBoxPromptOrInfo.ShowDialog();
-		SceneController.Instance.LoadNewWorld(width, height, depth, seed, generatorFile, GenerateAsteroids.isOn);
+        SceneController.Instance.LoadNewWorld(width, height, depth, seed, generatorFile, GenerateAsteroids.isOn);
     }
 
     public void VerifyNumericInput(InputField input)
