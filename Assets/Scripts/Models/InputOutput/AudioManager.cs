aea6349b15ce5070ce8e47cbb51cb102568acd0a
BraedonWooding <braedonww@gmail.com>
2017-01-14 15:41:06 +1100

    Releases all channel groups

--- Assets/Scripts/Models/InputOutput/AudioManager.cs
+++ Assets/Scripts/Models/InputOutput/AudioManager.cs
@@ -122,7 +122,13 @@ public class AudioManager
     public static void Destroy()
     {
         SoundSystem.close();
-        master.release();
+
+        // This will also release master, so we don't have to call master.release();
+        foreach (string key in channelGroups.Keys)
+        {
+            channelGroups[key].release();
+        }
+
         SoundSystem.release();
 
         SoundSystem = null;
