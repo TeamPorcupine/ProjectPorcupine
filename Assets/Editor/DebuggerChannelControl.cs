afc961a24efa09947cf350616a9b895e709b0867
Brian Jubelirer <brian2386@gmail.com>
2017-01-16 18:53:37 -0500

    stylecop

--- Assets/Editor/DebuggerChannelControl.cs
+++ Assets/Editor/DebuggerChannelControl.cs
@@ -111,6 +111,7 @@ public class DebuggerChannelControl : EditorWindow, IHasCustomMenu
             {
                 Awake();
             }
+            
             EditorUtility.SetDirty(channelSettings);
         }
 
