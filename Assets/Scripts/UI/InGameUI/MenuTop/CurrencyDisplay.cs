TeamPorcupine/ProjectPorcupine

commit 903e0711ba12a59270138d4ec19fe435cbf4dbaa

Money counter (#1722)

Assets/Scripts/UI/InGameUI/MenuTop/CurrencyDisplay.cs
@@ -1,0 +1,38 @@
+﻿#region License
+// ====================================================
+// Project Porcupine Copyright(C) 2016 Team Porcupine
+// This program comes with ABSOLUTELY NO WARRANTY; This is free software, 
+// and you are welcome to redistribute it under certain conditions; See 
+// file LICENSE, which is part of this source code package, for details.
+// ====================================================
+#endregion
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrencyDisplay : MonoBehaviour
+{
+    public Text text;
+    private string[] currencies;
+
+    private void Start()
+    {
+        currencies = World.Current.Wallet.GetCurrencyNames();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {    
+        StringBuilder content = new StringBuilder();
+
+        // Populate the text box
+        foreach (string currency in currencies)
+        {
+            content.Append(currency + ":");
+            content.Append(World.Current.Wallet[currency].Balance);
+            content.AppendLine();
+        }
+        
+        text.text = content.ToString();
+    }
+}

Assets/Resources/UI/MenuTop.prefab
@@ -11,6 +11,24 @@ Prefab:
   m_ParentPrefab: {fileID: 0}
   m_RootGameObject: {fileID: 1000012812495066}
   m_IsPrefabParent: 1
+--- !u!1 &1000011211211368
+GameObject:
+  m_ObjectHideFlags: 0
+  m_PrefabParentObject: {fileID: 0}
+  m_PrefabInternal: {fileID: 100100000}
+  serializedVersion: 4
+  m_Component:
+  - 224: {fileID: 224000010254345086}
+  - 222: {fileID: 222000014109352946}
+  - 114: {fileID: 114000010676539892}
+  - 114: {fileID: 114000011382288030}
+  m_Layer: 5
+  m_Name: CurrencyDisplay
+  m_TagString: Untagged
+  m_Icon: {fileID: 0}
+  m_NavMeshLayer: 0
+  m_StaticEditorFlags: 0
+  m_IsActive: 1
 --- !u!1 &1000012812495066
 GameObject:
   m_ObjectHideFlags: 0
@@ -43,6 +61,51 @@ GameObject:
   m_NavMeshLayer: 0
   m_StaticEditorFlags: 0
   m_IsActive: 1
+--- !u!114 &114000010676539892
+MonoBehaviour:
+  m_ObjectHideFlags: 1
+  m_PrefabParentObject: {fileID: 0}
+  m_PrefabInternal: {fileID: 100100000}
+  m_GameObject: {fileID: 1000011211211368}
+  m_Enabled: 1
+  m_EditorHideFlags: 0
+  m_Script: {fileID: 708705254, guid: f5f67c52d1564df4a8936ccd202a3bd8, type: 3}
+  m_Name: 
+  m_EditorClassIdentifier: 
+  m_Material: {fileID: 0}
+  m_Color: {r: 1, g: 1, b: 1, a: 1}
+  m_RaycastTarget: 1
+  m_OnCullStateChanged:
+    m_PersistentCalls:
+      m_Calls: []
+    m_TypeName: UnityEngine.UI.MaskableGraphic+CullStateChangedEvent, UnityEngine.UI,
+      Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+  m_FontData:
+    m_Font: {fileID: 12800000, guid: 27e088ad67e5c94499dd91fedaea0fd4, type: 3}
+    m_FontSize: 14
+    m_FontStyle: 0
+    m_BestFit: 0
+    m_MinSize: 10
+    m_MaxSize: 40
+    m_Alignment: 1
+    m_AlignByGeometry: 0
+    m_RichText: 1
+    m_HorizontalOverflow: 0
+    m_VerticalOverflow: 0
+    m_LineSpacing: 1
+  m_Text: "Quill Corp Bucks:150\r\nInter-Galactic Credits:0\r\n"
+--- !u!114 &114000011382288030
+MonoBehaviour:
+  m_ObjectHideFlags: 1
+  m_PrefabParentObject: {fileID: 0}
+  m_PrefabInternal: {fileID: 100100000}
+  m_GameObject: {fileID: 1000011211211368}
+  m_Enabled: 1
+  m_EditorHideFlags: 0
+  m_Script: {fileID: 11500000, guid: 2c2a81f1ede1b5b4b9269c4ccf72451e, type: 3}
+  m_Name: 
+  m_EditorClassIdentifier: 
+  text: {fileID: 114000010676539892}
 --- !u!114 &114000012707566334
 MonoBehaviour:
   m_ObjectHideFlags: 1
@@ -77,6 +140,30 @@ MonoBehaviour:
   m_Script: {fileID: 11500000, guid: 709d7ff1cc960724299a36a7cdc43f2f, type: 3}
   m_Name: 
   m_EditorClassIdentifier: 
+--- !u!222 &222000014109352946
+CanvasRenderer:
+  m_ObjectHideFlags: 1
+  m_PrefabParentObject: {fileID: 0}
+  m_PrefabInternal: {fileID: 100100000}
+  m_GameObject: {fileID: 1000011211211368}
+--- !u!224 &224000010254345086
+RectTransform:
+  m_ObjectHideFlags: 1
+  m_PrefabParentObject: {fileID: 0}
+  m_PrefabInternal: {fileID: 100100000}
+  m_GameObject: {fileID: 1000011211211368}
+  m_LocalRotation: {x: -0, y: -0, z: -0, w: 1}
+  m_LocalPosition: {x: 0, y: 0, z: 0}
+  m_LocalScale: {x: 1, y: 1, z: 1}
+  m_LocalEulerAnglesHint: {x: 0, y: 0, z: 0}
+  m_Children: []
+  m_Father: {fileID: 224000010732401530}
+  m_RootOrder: 1
+  m_AnchorMin: {x: 0, y: 1}
+  m_AnchorMax: {x: 0, y: 1}
+  m_AnchoredPosition: {x: 99, y: -57}
+  m_SizeDelta: {x: 199, y: 47}
+  m_Pivot: {x: 0.5, y: 0.5}
 --- !u!224 &224000010732401530
 RectTransform:
   m_ObjectHideFlags: 1
@@ -89,12 +176,13 @@ RectTransform:
   m_LocalEulerAnglesHint: {x: 0, y: 0, z: 0}
   m_Children:
   - {fileID: 224000013352927720}
+  - {fileID: 224000010254345086}
   m_Father: {fileID: 0}
   m_RootOrder: 0
   m_AnchorMin: {x: 0, y: 0.7846835}
   m_AnchorMax: {x: 0.62, y: 1}
-  m_AnchoredPosition: {x: 0.25, y: 0}
-  m_SizeDelta: {x: 1.5, y: 1}
+  m_AnchoredPosition: {x: 0, y: 0}
+  m_SizeDelta: {x: 0, y: 0}
   m_Pivot: {x: 0.5, y: 1}
 --- !u!224 &224000013352927720
 RectTransform:
@@ -111,6 +199,6 @@ RectTransform:
   m_RootOrder: 0
   m_AnchorMin: {x: 0, y: 0}
   m_AnchorMax: {x: 1, y: 1}
-  m_AnchoredPosition: {x: 0.00051116943, y: 0}
+  m_AnchoredPosition: {x: 0.00051116943, y: -63}
   m_SizeDelta: {x: 0, y: 0}
   m_Pivot: {x: 0.5, y: 0.5}

Assets/Resources/UI/TextPerformanceComponentUI.prefab
@@ -24,7 +24,7 @@ GameObject:
   - 114: {fileID: 114000011387328838}
   m_Layer: 5
   m_Name: TextPerformanceComponentUI
-  m_TagString: Untagged
+  m_TagString: PerformanceUI
   m_Icon: {fileID: 0}
   m_NavMeshLayer: 0
   m_StaticEditorFlags: 0

Assets/Scripts/Models/Trade/Trade.cs
@@ -19,7 +19,7 @@ public class Trade
     /// <summary>
     /// Create a trade instance between two trader
     /// Creating a trade will scan the inventory of both trader and assign a trade price to each inventory
-    /// A trade is a simple pivot on 2 Stock in a format easier to display to a humman.
+    /// A trade is a simple pivot on 2 Stock in a format easier to display to a human.
     /// </summary>
     public Trade(Trader player, Trader trader)
     {
@@ -55,7 +55,7 @@ public class Trade
     }
 
     /// <summary>
-    /// Check if both trader in the trade have enougth currency to complete the deal.
+    /// Check if both trader in the trade have enough currency to complete the deal.
     /// </summary>
     /// <returns></returns>
     public bool IsValid()

Assets/Scripts/Models/Trade/Wallet.cs
@@ -8,6 +8,7 @@
 #endregion
 
 using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json.Linq;
 
 public class Wallet
@@ -37,7 +38,12 @@ public class Wallet
 
             return null;
         }
-    } 
+    }
+
+    public string[] GetCurrencyNames()
+    {
+        return currencies.Keys.ToArray();
+    }
 
     /// <summary>
     /// Adds a currency with the given name and balance.

Assets/Scripts/UI/InGameUI/MenuTop.meta
@@ -1,0 +1,9 @@
+fileFormatVersion: 2
+guid: f5105a5379f432547ab55acfdf066b9f
+folderAsset: yes
+timeCreated: 1482795167
+licenseType: Free
+DefaultImporter:
+  userData: 
+  assetBundleName: 
+  assetBundleVariant: 

Assets/Scripts/UI/InGameUI/MenuTop/CurrencyDisplay.cs.meta
@@ -1,0 +1,12 @@
+fileFormatVersion: 2
+guid: 2c2a81f1ede1b5b4b9269c4ccf72451e
+timeCreated: 1482795191
+licenseType: Free
+MonoImporter:
+  serializedVersion: 2
+  defaultReferences: []
+  executionOrder: 0
+  icon: {instanceID: 0}
+  userData: 
+  assetBundleName: 
+  assetBundleVariant: 

Assets/Scripts/UI/InGameUI/PerformanceHUD/PerformanceHUDManager.cs
@@ -42,7 +42,10 @@ public class PerformanceHUDManager : MonoBehaviour
         // Clear
         foreach (Transform child in rootObject.transform)
         {
-            Destroy(child.gameObject);
+            if (child.tag == "PerformanceUI")
+            {
+                Destroy(child.gameObject);
+            }
         }
 
         // Draw

commit 37177fa1a426461e2ed32df4d7c1217176bcb3c6

Disable currency UI in main menu (#1771)
temporary fix to bug introduced in #1722

Assets/Scripts/UI/InGameUI/MenuTop/CurrencyDisplay.cs
@@ -1,4 +1,4 @@
-﻿#region License
+#region License
 // ====================================================
 // Project Porcupine Copyright(C) 2016 Team Porcupine
 // This program comes with ABSOLUTELY NO WARRANTY; This is free software, 
@@ -8,6 +8,7 @@
 #endregion
 using System.Text;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class CurrencyDisplay : MonoBehaviour
@@ -17,6 +18,12 @@ public class CurrencyDisplay : MonoBehaviour
 
     private void Start()
     {
+        if (SceneManager.GetActiveScene().name != "_World")
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
         currencies = World.Current.Wallet.GetCurrencyNames();
     }
 