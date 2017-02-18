0f870f06bc1f778905b2f7ea12f1dd90a0218fb5
Koosemose <koosemose@gmail.com>
2017-02-17 23:45:25 -0500

    Stylecop

--- Assets/UIRescaler.cs
+++ Assets/UIRescaler.cs
@@ -1,27 +1,38 @@
-﻿using System.Collections.Generic;
+﻿#region License
+// ====================================================
+// Project Porcupine Copyright(C) 2016 Team Porcupine
+// This program comes with ABSOLUTELY NO WARRANTY; This is free software,
+// and you are welcome to redistribute it under certain conditions; See
+// file LICENSE, which is part of this source code package, for details.
+// ====================================================
+#endregion
+
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIRescaler : MonoBehaviour {
+public class UIRescaler : MonoBehaviour 
+{
     public int lastScreenWidth = 0;
 
-    void Start(){
+    public void Start()
+    {
         lastScreenWidth = Screen.width;
         StartCoroutine("AdjustScale");
         Debug.LogWarning(lastScreenWidth);
     }
 
-    void Update(){
-        if( lastScreenWidth != Screen.width ){
+    public void Update()
+    {
+        if (lastScreenWidth != Screen.width)
+        {
             lastScreenWidth = Screen.width;
             StartCoroutine("AdjustScale");
         }
-
     }
 
-    IEnumerator AdjustScale(){
-        Debug.LogWarning(lastScreenWidth);
+    private IEnumerator AdjustScale()
+    {
         this.GetComponent<CanvasScaler>().scaleFactor = Screen.width / 1920f;
         yield return null;
     }
