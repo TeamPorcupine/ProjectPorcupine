8cb63cf4f73c54ae6e187c767a7db0c7cb79d7df
Koosemose <koosemose@gmail.com>
2017-02-17 23:35:19 -0500

    Canvas Rescaler

---
+++ Assets/UIRescaler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIRescaler : MonoBehaviour {
+    public int lastScreenWidth = 0;
+
+    void Start(){
+        lastScreenWidth = Screen.width;
+        StartCoroutine("AdjustScale");
+        Debug.LogWarning(lastScreenWidth);
+    }
+
+    void Update(){
+        if( lastScreenWidth != Screen.width ){
+            lastScreenWidth = Screen.width;
+            StartCoroutine("AdjustScale");
+        }
+
+    }
+
+    IEnumerator AdjustScale(){
+        Debug.LogWarning(lastScreenWidth);
+        this.GetComponent<CanvasScaler>().scaleFactor = Screen.width / 1920f;
+        yield return null;
+    }
+}
