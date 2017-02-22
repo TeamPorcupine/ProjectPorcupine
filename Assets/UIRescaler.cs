0939ebf84b464ba7dabf14a0808930b41cd18e23
Koosemose <koosemose@gmail.com>
2017-02-21 23:12:20 -0500

    DPI- based scaling

--- Assets/UIRescaler.cs
+++ Assets/UIRescaler.cs
@@ -13,13 +13,48 @@ using UnityEngine.UI;
 
 public class UIRescaler : MonoBehaviour 
 {
-    public int lastScreenWidth = 0;
+    private const float MaxScale = 2f;
+    private const float MinScale = .5f;
+
+    private int lastScreenWidth = 0;
+    private float dpi;
+    private float scale = 1f;
+
+    public float Scale 
+    {
+        get
+        {
+            return scale;
+        }
+
+        set
+        {
+            if (value > MaxScale)
+            {
+                scale = MaxScale;
+            }
+            else if (value < MinScale)
+            {
+                scale = MinScale;
+            }
+            else 
+            {
+                scale = value;
+            }
+        }
+    }
 
     public void Start()
     {
         lastScreenWidth = Screen.width;
-        StartCoroutine("AdjustScale");
-        Debug.LogWarning(lastScreenWidth);
+        dpi = Screen.dpi;
+        if (dpi.AreEqual(0f)) 
+        {
+            // Screen dpi may sometimes not be able to get the correct dpi, so we choose a reasonable default in that case.
+            dpi = 96f;
+        }
+
+        AdjustScale();
     }
 
     public void Update()
@@ -27,13 +62,12 @@ public class UIRescaler : MonoBehaviour
         if (lastScreenWidth != Screen.width)
         {
             lastScreenWidth = Screen.width;
-            StartCoroutine("AdjustScale");
+            AdjustScale();
         }
     }
 
-    private IEnumerator AdjustScale()
+    private void AdjustScale()
     {
-        this.GetComponent<CanvasScaler>().scaleFactor = Screen.width / 1920f;
-        yield return null;
+        this.GetComponent<CanvasScaler>().scaleFactor = (Screen.width / 1920f) * (dpi / 96) * scale;
     }
 }
