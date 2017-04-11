36efe02e745d438fa608aa581fa720c3b17eb022
Braedon <Braedonww@gmail.com>
2017-04-11 12:21:56 +1000

    Stylecop final (#1798)
    * Final Stylecop

--- Assets/Editor/UnitTests/Models/Area/AtmosphereComponentTest.cs
+++ Assets/Editor/UnitTests/Models/Area/AtmosphereComponentTest.cs
@@ -1,9 +1,18 @@
-﻿using System;
+#region License
+// ====================================================
+// Project Porcupine Copyright(C) 2016 Team Porcupine
+// This program comes with ABSOLUTELY NO WARRANTY; This is free software,
+// and you are welcome to redistribute it under certain conditions; See
+// file LICENSE, which is part of this source code package, for details.
+// ====================================================
+#endregion
+
+using System;
 using NUnit.Framework;
 
 public class AtmosphereComponentTest
 {
-    AtmosphereComponent empty1, empty2, notEmpty1, notEmpty2;
+    private AtmosphereComponent empty1, empty2, notEmpty1, notEmpty2;
 
     [SetUp]
     public void Setup()
